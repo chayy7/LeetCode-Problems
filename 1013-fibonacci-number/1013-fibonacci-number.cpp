001cb3260e67d89e9607786d20962dd552b6db6e
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-10-24 22:02:19 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.97%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
 
-    int fibb(vector<int>& dp,int n){
+    int fibb(vector<int>& dp, int n){
         if(n <= 1){
             return n;
         }
 
-        if(dp[n] != -1){
-            return dp[n];
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
 
-        return dp[n] = fibb(dp,n-1)+fibb(dp,n-2);
+        return dp[n];
     }
     int fib(int n) {
-        
-
-        vector<int> dp(n+1,-1);
+        if(n <= 1){
+            return n;
+        }
+        vector<int> dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = 1;
         return fibb(dp,n);
-        
     }
 };
