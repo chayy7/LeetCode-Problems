22a6e58a1ab17fd2375cdf477846570d21f74743
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-10-24 20:49:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+
+        vector<int> dp(n+1,-1);
+        if(n <= 1){
+            return n;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = fib(n-1)+fib(n-2);
+    }
+};
