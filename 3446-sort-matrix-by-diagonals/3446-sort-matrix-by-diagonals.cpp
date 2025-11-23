40420362006b15907c86ad5076b83791b63c3ae7
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-11-23 15:42:39 +0530

    Time: 7 ms (70.74%), Space: 45.4 MB (14.41%) - LeetHub

---
+++ 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        map<int,vector<int>> mpp;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                mpp[i-j].push_back(grid[i][j]);
+            }
+        }
+
+        for(auto &i:mpp){
+            if(i.first < 0){
+                sort(i.second.begin(),i.second.end(),greater<int>());
+            }else{
+                sort(i.second.begin(),i.second.end());
+            }
+        }
+
+        for(int i=0;i<grid.size();i++){
+            for(int j= 0;j<grid[0].size();j++){
+                
+                grid[i][j] = mpp[i-j].back();
+                mpp[i-j].pop_back();
+            }
+        }
+
+        return grid;
+    }
+};
