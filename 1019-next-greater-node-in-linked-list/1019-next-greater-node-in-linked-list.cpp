e12b10c91828098f5295048c5e42b33800ccca2d
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-11-26 17:11:46 +0530

    Time: 824 ms (7.95%), Space: 48.5 MB (5.02%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> v;
+        ListNode* temp=head;
+        while(temp){
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+
+
+        vector<int> ans;
+        for(int i=0;i<v.size();i++){
+            int maxi=v[i];
+            for(int j=i+1;j<v.size();j++){
+                if(maxi < v[j]){
+                    maxi=max(v[j],maxi);
+                    break;
+                }
+                
+                // break;
+            }
+
+            if(v[i] == maxi) {
+                ans.push_back(0);
+            }else{
+
+                ans.push_back(maxi);
+            }
+            
+
+        }
+
+        for(auto i:ans){
+            cout << i << " ";
+        }
+
+        return ans;
+    }
+};
