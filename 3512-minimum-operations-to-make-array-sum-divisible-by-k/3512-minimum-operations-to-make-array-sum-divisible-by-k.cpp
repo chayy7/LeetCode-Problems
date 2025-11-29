81de1f13ae6eebf58037fc7f1ef748c76263618e
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-11-29 22:05:45 +0530

    Time: 0 ms (100%), Space: 45.4 MB (3.18%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -1,10 +1,39 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int sum=0;
-        for(auto i:nums){
-            sum+=i;
+        int sum = 0;
+        int count =0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
         }
+        // cout << sum;
+        // if (sum % k == 0) {
+        //     return 0;
+        // }else if(sum < k){
+        //     return sum;
+        // }
+        // else {
+        //     while(sum % k != 0) {
+        //         sum--;
+        //         count++;
+        //     }
+        // }
+
+        // return count - 1;
         return sum%k;
     }
 };
+
+
+
+
+
+/* Shall i say?? we ll move to another question
+can u fix this? code
+
+
+actually u understood the question wrong i guess
+
+ */
+
+
