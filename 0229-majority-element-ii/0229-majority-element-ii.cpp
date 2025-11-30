e30ad4088d60fa2fe97926f51d4a2591d90eb0ac
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-11-30 21:28:38 +0530

    Time: 11 ms (21.66%), Space: 27.1 MB (5.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        vector <int> result;
-        int duplicate = n/3;
-        for(int i=0;i<n;i++){
-            int count = 1;
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-          
-            bool isThere = false;
-            for(int k=0;k<result.size();k++){
-                if(result[k] == nums[i]){
-                    isThere = true;
-                    break;
-                }
-            }
-            if(count > duplicate && !isThere){
-                result.push_back(nums[i]);
-            }
             
+        map<int,int> mpp;
+        for(auto i:nums){
+            mpp[i]++;
+        }
+        vector<int> ans;
+        for(auto i:mpp){
+            if(i.second > nums.size()/3){
+                ans.push_back(i.first);
+            }
         }
         
-       return result; 
+        return ans;
+        
+             
+        
+ 
+        // int n = nums.size();
+        //     vector <int> result;
+        //     int duplicate = n/3;
+        //     for(int i=0;i<n;i++){
+        //         int count = 1;
+        //         for(int j=i+1;j<n;j++){
+        //             if(nums[i]==nums[j]){
+        //                 count++;
+        //             }
+        //         }
+            
+        //         bool isThere = false;
+        //         for(int k=0;k<result.size();k++){
+        //             if(result[k] == nums[i]){
+        //                 isThere = true;
+        //                 break;
+        //             }
+        //         }
+        //         if(count > duplicate && !isThere){
+        //             result.push_back(nums[i]);
+        //         }
+                
+        //     }
+            
+        //    return result; 
     }
 };
