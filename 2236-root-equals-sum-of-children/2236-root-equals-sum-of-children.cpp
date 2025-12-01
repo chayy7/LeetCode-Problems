00c6b4da43b7407aeba60e3cd29008856e4831ea
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-01 09:23:20 +0530

    Time: 0 ms (100%), Space: 15.1 MB (31.66%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -11,32 +11,49 @@
  */
 class Solution {
 public:
+
+    void inorder(TreeNode* root,vector<int>& v){
+        if(root == nullptr) return ;
+
+
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
     bool checkTree(TreeNode* root) {
-        vector<int> ans;
+        // vector<int> ans;
+        // if(root == nullptr) return false;
+
+        // queue<TreeNode* > q;
+        // q.push(root);
+
+        // while(!q.empty()){
+        //     int size=q.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode* front=q.front();
+        //         q.pop();
+
+        //         ans.push_back(front->val);
+        //         if(front->left) q.push(front->left);
+        //         if(front->right) q.push(front->right);
+        //     }
+        // }
+        // int sum=0;
+        // for(auto i:ans){
+        //     cout << i << " ";
+        //     sum += i;
+        // }
+
+        // if((sum - ans[0]) == ans[0]) return true;
+
+        // return false;
         if(root == nullptr) return false;
 
-        queue<TreeNode* > q;
-        q.push(root);
-
-        while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* front=q.front();
-                q.pop();
-
-                ans.push_back(front->val);
-                if(front->left) q.push(front->left);
-                if(front->right) q.push(front->right);
-            }
+        vector<int> v;
+        inorder(root,v);
+        if((v[0]+v[2]) == v[1]){
+            return true;
         }
-        int sum=0;
-        for(auto i:ans){
-            cout << i << " ";
-            sum += i;
-        }
-
-        if((sum - ans[0]) == ans[0]) return true;
-
         return false;
     } 
 };
