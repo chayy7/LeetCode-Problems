2b46cd74873fa25e76ffe80b4dc27ecee8ac7b65
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-02 21:50:53 +0530

    Time: 0 ms (100%), Space: 32.2 MB (6.22%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void traverse(TreeNode* root, vector<int>& arr){ 
+        if(root == NULL) return;
+        traverse(root -> left, arr);
+        arr.push_back(root -> val);
+        traverse(root -> right, arr);
+    }
+    int countNodes(TreeNode* root) {
+        vector <int> emptyArr;
+        traverse(root, emptyArr);
+        return emptyArr.size();
+    }
+};
