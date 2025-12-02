34224a3ae9d05549976d5b97bb305ec4615bdd99
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-02 22:15:50 +0530

    Time: 0 ms (100%), Space: 9.3 MB (15.88%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.cpp
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.cpp
@@ -10,11 +10,6 @@ public:
         vector<int> arr;
         traverse(root, arr);
         long long min = LLONG_MAX; 
-        /* 
-            when we find min , then INT_MAX;
-            when we find max , then INT_MIN;
-            this is always confusing for me, hmm hmm
-         */
         for(int i=0;i<arr.size();i++){
             if(arr[i] < min){
                 min = arr[i];
@@ -30,18 +25,3 @@ public:
         return secMin; 
     }
 };
-
-
-/* 
-
-    our whole code is right, but why the test cases are not accepting?
-    
-    the answer is simple, we used INT instead of LONG LONG
-    
-    the test cases consists of LONG LONG 
-    
-    so if we keep LONG LONG instead of int, then it ll be ok
-
-
-
- */
