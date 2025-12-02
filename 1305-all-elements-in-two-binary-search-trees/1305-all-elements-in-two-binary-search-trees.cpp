98b78413c6e7c4bd681c37a46f6b3b5fea7d0a60
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-02 22:27:47 +0530

    Time: 4 ms (76.29%), Space: 88.6 MB (20.95%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,44 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void inorder1(TreeNode* root1, vector<int>& v1){
-        if(root1 == nullptr) return;
-
-        inorder1(root1->left,v1);
-        v1.push_back(root1->val);
-        inorder1(root1->right,v1);
+    void traverse(TreeNode* root, vector<int>& arr1){ 
+        if(root == NULL) return;
+        traverse(root -> left, arr1);
+        arr1.push_back(root -> val);
+        traverse(root -> right, arr1);
     }
-    void inorder2(TreeNode* root2, vector<int>& v2){
-        if(root2 == nullptr) return;
 
-        inorder1(root2->left,v2);
-        v2.push_back(root2->val);
-        inorder1(root2->right,v2);
-    }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> v1;
-        vector<int> v2;
-        inorder1(root1,v1);
-        inorder2(root2,v2);
-
-        vector<int> ans;
-        for(auto i:v1){
-            ans.push_back(i);
+        vector <int> arr1;
+        vector <int> arr2;
+        
+        traverse(root1, arr1);
+        traverse(root2, arr2);
+        
+        vector <int> mergeArr;
+        for(auto i:arr1){
+            mergeArr.push_back(i);
         }
-        for(auto i:v2){
-            ans.push_back(i);
+        for(auto i:arr2){
+            mergeArr.push_back(i);
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+        
+        sort(mergeArr.begin(),mergeArr.end());
+        return mergeArr;       
     }
 };
