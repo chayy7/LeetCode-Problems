0d3570f7a516612d4eac8ce1e63465f3abb34599
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-02 22:14:42 +0530

    Time: 0 ms (100%), Space: 9.4 MB (15.88%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.cpp
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.cpp
@@ -1,46 +1,47 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void inorder(vector<int>& v, TreeNode* root){
-        if(root == nullptr) return ;
-
-        inorder(v,root->left);
-        v.push_back(root->val);
-        inorder(v,root->right);
-        
+    void traverse(TreeNode* root, vector<int>& arr){ 
+        if(root == NULL) return;
+        traverse(root -> left, arr);
+        arr.push_back(root -> val);
+        traverse(root -> right, arr);
     }
     int findSecondMinimumValue(TreeNode* root) {
-        vector<int> v;
-        inorder(v,root);
-
-        long long  min = LLONG_MAX;
-        for(auto i: v){
-            if(i < min){
-                min = i;
+        vector<int> arr;
+        traverse(root, arr);
+        long long min = LLONG_MAX; 
+        /* 
+            when we find min , then INT_MAX;
+            when we find max , then INT_MIN;
+            this is always confusing for me, hmm hmm
+         */
+        for(int i=0;i<arr.size();i++){
+            if(arr[i] < min){
+                min = arr[i];
             }
         }
-
-        long long smin = LLONG_MAX;
-        for(auto i: v){
-            if(i < smin && i != min){
-                smin = i;
+        long long secMin = LLONG_MAX;
+        for(int i=0;i<arr.size();i++){
+            if((arr[i] < secMin) && (arr[i] != min)){
+                secMin = arr[i];
             }
         }
-
-        cout << smin;
-        if(smin == LLONG_MAX){
-            return -1;
-        }
-        return smin;
+        if(secMin == LLONG_MAX)return -1;
+        return secMin; 
     }
 };
+
+
+/* 
+
+    our whole code is right, but why the test cases are not accepting?
+    
+    the answer is simple, we used INT instead of LONG LONG
+    
+    the test cases consists of LONG LONG 
+    
+    so if we keep LONG LONG instead of int, then it ll be ok
+
+
+
+ */
