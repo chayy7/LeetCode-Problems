07bc797e711aeb1f3b55c3b171301ecf7a6edef7
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-12 17:41:34 +0530

    Time: 0 ms (100%), Space: 14.8 MB (98.41%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -4,20 +4,17 @@ public:
         int n=mat.size();
         int m=mat[0].size();
 
-        vector<int> v;
+        // vector<int> v;
+
+        int sum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if((i-j) == 0 || (i+j)%(n-1) == 0){
-                    v.push_back(mat[i][j]);
+                    sum += mat[i][j];
                 }
             }
         }
-        int sum=0;
-        for(auto i:v){
-            sum += i;
-            cout << i << " ";
-        }
-
+        
         return sum;
 
 
