d176b3796d591a6cc35f83bd34e727ddf1db0537
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-13 06:17:50 +0530

    Time: 0 ms (100%), Space: 17.4 MB (85.96%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> v;
-        int n = nums.size();
-        int l=0,r=n-1,fi=-1,la=-1;
+        if (nums.empty()) return {-1, -1};
+        int n=nums.size();
+        int l=0;
+        int r= n-1;
+
+        int lwr = -1;
+        int upr = -1;
+
         while(l <= r){
-            int mid = l+(r-l)/2;
-            if(nums[mid] == target){
-                fi=mid;;
-                r=mid-1;
+            int mid = (l+r)/2;
+
+            if(nums[mid] >= target){
+                r = mid-1;
             }else if(nums[mid] < target){
                 l=mid+1;
-            }else{
-                r=mid-1;
+            } 
+        }
+        if(l == n || nums[l] != target){
+            return {-1,-1};
+        }
+        lwr= l;
+
+        cout << lwr;
+
+        int l1=0;
+        int r1 = n-1;
+        while(l1 <= r1){
+            int mid = (l1+r1)/2;
+            if(nums[mid] > target){
+                r1 = mid-1;
+            }else if(nums[mid] <= target){
+                l1 = mid+1;
             }
         }
-        l=0,r=n-1;
-        while(l <= r){
-            int mid = l+(r-l)/2;
-            if(nums[mid] == target){
-                la=mid;
-                l=mid+1;
-            }else if(nums[mid] < target){
-                l=mid+1;
-            }else{
-                r=mid-1;
-            }
-        }
-        v.push_back(fi);
-        v.push_back(la);
-        return v;
+
+        upr=r1;
+        cout << upr;
+        return {lwr,upr};
+
     }
 };
