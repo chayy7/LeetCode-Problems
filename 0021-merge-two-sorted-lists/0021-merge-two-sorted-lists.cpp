6d422654888be9577abd306cb4ba0b9d58098b8d
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-15 20:33:04 +0530

    Time: 0 ms (100%), Space: 19.9 MB (9.22%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,32 +11,38 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *head= new ListNode(0);
-        ListNode *tail=head;
-
-        while(list1 && list2){
-            if(list1->val <= list2->val){
-                tail->next=list1;
-                list1=list1->next;
-                tail=tail->next;
-                tail->next=NULL;
-            }else{
-                tail->next=list2;
-                list2=list2->next;
-                tail=tail->next;
-                tail->next=NULL;
-
-            }
+        vector<int> v1;
+        ListNode* temp1=list1;
+        while(temp1){
+            v1.push_back(temp1->val);
+            temp1=temp1->next;
         }
 
-        if(list1){
-            tail->next=list1;
-        }else{
-            tail->next=list2;
+        vector<int> v2;
+        ListNode* temp2=list2;
+        while(temp2){
+            v2.push_back(temp2->val);
+            temp2=temp2->next;
         }
-        tail=head;
-        head=head->next;
-        delete tail;
-        return head;
+
+        vector<int> v3;
+        for(auto i:v1){
+            v3.push_back(i);
+        }
+        for(auto i:v2){
+            v3.push_back(i);
+        }
+
+        sort(v3.begin(),v3.end());
+
+        if(v3.empty()) return nullptr;
+        ListNode* c = new ListNode(v3[0]);
+        ListNode* curr = c;
+
+        for(int i=1;i<v3.size();i++){
+            curr->next=  new ListNode(v3[i]);
+            curr= curr->next;
+        }
+        return c;
     }
 };
