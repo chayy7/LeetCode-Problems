64fc8f8f97fb39f4804c227803d93b997df6f84e
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-15 21:27:55 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.33%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int idx = 0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i] != 0){
+                swap(arr[i],arr[idx]);
+                idx++;
+            }
+        }
+    }
+};
