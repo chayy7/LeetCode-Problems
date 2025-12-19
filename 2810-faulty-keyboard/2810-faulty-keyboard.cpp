d35b0bfb228d3878e9a2269ebccd8525757bd0e9
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-20 05:28:57 +0530

    Time: 0 ms (100%), Space: 9.2 MB (81.99%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string finalString(string s) {
+        int n=s.size();
+        string ans = "";
+        for(int i=0;i<n;i++){
+            if(s[i] == 'i'){
+                reverse(ans.begin(),ans.end());
+            }else{
+                ans += s[i];
+            }
+        }
+
+        return ans;
+    }
+};
