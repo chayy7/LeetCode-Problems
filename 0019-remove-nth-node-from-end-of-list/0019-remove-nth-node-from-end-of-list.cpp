cad7e26c77112d4c54f7a6a4c9e540b484228868
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-21 22:27:47 +0530

    Time: 3 ms (1.63%), Space: 15.2 MB (24.89%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,37 +1,61 @@
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         ListNode* prev = -1;
+//         ListNode* justPrev = head;
+//         ListNode* curr = temp->next;
+         
+//         int len = 0;
+//         ListNode* temp = head;
+//         while(temp){
+//             len++;
+//             temp = temp->next;
+//         }
+//         int deleteNode = len - n;
+//         while()
+//         // 1 min baby, i ll comment this
+//     }
+// };
+
+
+
+
+
+
+
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        vector<int> v;
         ListNode* temp=head;
-        int cnt=0;
         while(temp){
-            cnt++;
+            v.push_back(temp->val);
             temp=temp->next;
         }
         
-        vector<int> v;
-        int s = cnt-n;
-        if(s == 0){
-            temp=head;
-            head= head->next;
-            delete temp;
-            return head;
+        int s = v.size()-n;
+        
+        vector<int> v1;
+        for(int i=0;i<v.size();i++){
+            if(i != s) v1.push_back(v[i]);
         }
         
-        ListNode* prev=nullptr;
-        ListNode* curr=head;
-        while(s--){
-            prev=curr;
+        if (v1.size() == 0) return NULL;
+
+        
+        // for(auto i:v1){
+        //     cout << i << " ";
+        // }
+        
+        ListNode* newll = new ListNode(v1[0]);
+        ListNode* curr= newll;
+        for(int i=1;i<v1.size();i++){
+            curr->next = new ListNode(v1[i]);
             curr=curr->next;
         }
         
-        prev->next = curr->next;
-        delete curr;
         
-        
-                
-        return head;
-        
+        return newll;
     }
-}; 
-
-// let's do thiis? idk ll..fine i ll try it..ok naa yes
+};
