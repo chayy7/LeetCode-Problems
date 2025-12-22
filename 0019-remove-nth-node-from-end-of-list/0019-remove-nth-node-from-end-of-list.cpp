a245b719a6c1142872d5c8d6d4b45ab5625daae0
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-23 05:14:46 +0530

    Time: 0 ms (100%), Space: 15 MB (24.87%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,61 +1,37 @@
-// class Solution {
-// public:
-//     ListNode* removeNthFromEnd(ListNode* head, int n) {
-//         ListNode* prev = -1;
-//         ListNode* justPrev = head;
-//         ListNode* curr = temp->next;
-         
-//         int len = 0;
-//         ListNode* temp = head;
-//         while(temp){
-//             len++;
-//             temp = temp->next;
-//         }
-//         int deleteNode = len - n;
-//         while()
-//         // 1 min baby, i ll comment this
-//     }
-// };
-
-
-
-
-
-
-
-
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        vector<int> v;
-        ListNode* temp=head;
+        ListNode* prev = NULL;
+        ListNode* justPrev = head;
+        ListNode* curr = head->next;
+         
+        int len = 0;
+        ListNode* temp = head;
         while(temp){
-            v.push_back(temp->val);
-            temp=temp->next;
+            len++;
+            temp = temp->next;
         }
-        
-        int s = v.size()-n;
-        
-        vector<int> v1;
-        for(int i=0;i<v.size();i++){
-            if(i != s) v1.push_back(v[i]);
+        int deleteNode = len - n;
+        if (deleteNode == 0) {
+            return head->next;
         }
-        
-        if (v1.size() == 0) return NULL;
-
-        
-        // for(auto i:v1){
-        //     cout << i << " ";
-        // }
-        
-        ListNode* newll = new ListNode(v1[0]);
-        ListNode* curr= newll;
-        for(int i=1;i<v1.size();i++){
-            curr->next = new ListNode(v1[i]);
-            curr=curr->next;
+        int i=1;
+        while(i < deleteNode){
+            prev = justPrev;
+            justPrev = justPrev -> next;
+            curr = curr->next;
+            i++;
         }
+        justPrev->next = curr->next;
+        return head;
         
-        
-        return newll;
     }
 };
+
+
+
+
+
+
+
+
