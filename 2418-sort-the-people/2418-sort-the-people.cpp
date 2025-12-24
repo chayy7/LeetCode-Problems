ce5e254e2b7b30bb49488e62cd5361b83a64ab04
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-24 21:57:57 +0530

    Time: 11 ms (60.05%), Space: 28.5 MB (44.55%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        map<int, string,greater<int>> mpp;
+        map<int, string, greater<int>> data; 
+        vector<string> ans; 
         for(int i=0;i<names.size();i++){
-            mpp[heights[i]] = names[i];
-        } 
-
-        vector<string> v;
-        for(auto i:mpp){
-            v.push_back(i.second);
+            data[heights[i]] = names[i];
         }
-
-        return v;
+        for(auto &i: data){
+            ans.push_back(i.second);
+        }
+        return ans;
     }
 };
