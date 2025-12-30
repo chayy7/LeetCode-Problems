f7a729c8f0cc4fa62aa430547f02640de6748c8f
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-12-30 22:03:10 +0530

    Time: 8 ms (6.34%), Space: 14.7 MB (7.64%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int> set;
+        int n =  set.size();
+        for(auto i:nums){
+            set.insert(i);
+        }
+        vector<int> v(set.begin(),set.end());
+        if(v.size() < 3){
+            return *max_element(v.begin(),v.end());
+        }
+
+            
+
+        return v[v.size()-3];   
+    }
+};
