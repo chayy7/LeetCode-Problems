989befd88f87cb1e4d7c5d3b801d5a7f01435856
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-03 18:09:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+
+        int l=0, r= 0;
+        int cnt =0;
+        while(l < g.size() && r < s.size()){
+            if(g[l] <= s[r]){
+                cnt++;
+                l++;
+                r++;
+            }else{
+                l++;
+                // r++;
+            }
+        }
+
+        return cnt;
+    }
+};
