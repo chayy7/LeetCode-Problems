df265a5cc7aa83b7425ad64ec9509138a902fbb9
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-03 22:05:43 +0530

    Time: 4 ms (59%), Space: 39.5 MB (68.65%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-
-    bool isprime(int n) {
-    if(n <= 1) return false;
-    if(n == 2) return true;
-    if(n % 2 == 0) return false;
-    
-    for(int i = 3; i * i <= n; i += 2) {
-        if(n % i == 0) return false;
-    }
-    return true;
+    bool isPrime(int num) {
+        if (num <= 1)
+            return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
     }
     int diagonalPrime(vector<vector<int>>& nums) {
-        vector<int> v;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums[0].size();j++){
-                if(i-j == 0 || i+j==nums[0].size()-1){
-                    if(isprime(nums[i][j])) v.push_back(nums[i][j]);
+        int maxPrime = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums[i].size(); j++) {
+                if (i == j) {
+                    if (isPrime(nums[i][j])) {
+                        maxPrime = max(maxPrime, nums[i][j]);
+                    }
+                }else if(i + j == nums.size() - 1){
+                    if (isPrime(nums[i][j])) {
+                        maxPrime = max(maxPrime, nums[i][j]);
+                    }
                 }
             }
         }
-
-        if(v.empty()) return 0;
-
-        return *max_element(v.begin(),v.end());
+        
+        return maxPrime;
     }
 };
+
