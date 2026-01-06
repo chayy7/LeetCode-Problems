3c3c6d944f94410fb9b686098d442e788a6227a1
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-06 21:46:03 +0530

    Time: 0 ms (100%), Space: 109.4 MB (79.94%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        vector<int> v;
         if(root == nullptr) return 0;
-
-        queue<TreeNode* > q;
+        queue<TreeNode*> q;
         q.push(root);
-
+        int maxSum = INT_MIN;
+        int ansLevel = 1;
+        int level = 1;
         while(!q.empty()){
-            int size = q.size();
-            vector<int> lvl;
-
-            for(int i=0;i<size;i++){
-                TreeNode* front = q.front();
+            int s = q.size();
+            int sum = 0;
+            while(s--){
+                TreeNode* temp = q.front(); 
                 q.pop();
-
-                lvl.push_back(front->val);
-                if(front->left) q.push(front->left);
-                if(front->right) q.push(front->right);
+                sum += temp->val;
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
             }
-
-            int s= 0;
-            for(auto i:lvl){
-                s += i;
+            if(sum > maxSum){
+                maxSum = sum;
+                ansLevel = level;
             }
-
-            v.push_back(s);
-            
+            level++;
         }
-
-        int maxi = *max_element(v.begin(),v.end());
-
-        int ans = 0;
-
-        for(int i=0;i<v.size();i++){
-            if(v[i] == maxi){
-                ans = i+1;
-                break;
-            }
-        }
-
-        return ans;
+        return ansLevel;
     }
 };
+
