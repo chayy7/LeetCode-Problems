43a7385a2b52270435aba002b3db1055c47af7e2
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-06 15:32:53 +0530

    Time: 4 ms (57.77%), Space: 113.3 MB (5.04%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -1,53 +1,45 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
+        vector<int> v;
         if(root == nullptr) return 0;
 
-        vector<int> ans;
-        queue<TreeNode*> q;
+        queue<TreeNode* > q;
         q.push(root);
 
         while(!q.empty()){
             int size = q.size();
             vector<int> lvl;
-            for(int i=0;i<size;i++){
-                auto front=q.front();
-                q.pop();
 
+            for(int i=0;i<size;i++){
+                TreeNode* front = q.front();
+                q.pop();
 
                 lvl.push_back(front->val);
                 if(front->left) q.push(front->left);
                 if(front->right) q.push(front->right);
             }
 
-            int sum=0;
+            int s= 0;
             for(auto i:lvl){
-                sum += i;
+                s += i;
             }
 
-            ans.push_back(sum);
+            v.push_back(s);
+            
         }
 
-        int maxi = ans[0];
-        int res = 0;
-        for(int i=1;i<ans.size();i++){
-            if(ans[i] > maxi){
-                maxi = ans[i];
-                res = i;
+        int maxi = *max_element(v.begin(),v.end());
+
+        int ans = 0;
+
+        for(int i=0;i<v.size();i++){
+            if(v[i] == maxi){
+                ans = i+1;
+                break;
             }
         }
 
-        return res+1;
+        return ans;
     }
 };
