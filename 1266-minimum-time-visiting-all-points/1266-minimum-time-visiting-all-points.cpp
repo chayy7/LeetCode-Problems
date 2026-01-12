739e6851b7585c971b338f19525371666be82ae6
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-12 20:35:13 +0530

    Time: 0 ms (100%), Space: 13.7 MB (88.14%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        // flattening the array
-        vector<int> v;
-        for(auto i:points){
-            for(auto j:i){
-                v.push_back(j);
-            }
-        }
-        
+        int row = points.size();
+        int col = points[0].size();
         int cnt = 0;
-        int l =2, r=l+1;
-        while(r < v.size()){
-            cnt += max(abs(v[l]-v[l-2]), abs(v[r]-v[r-2]));
-            l+=2, r+=2;
+        for (int i = 1; i < row; i++) {
+                int x = abs(points[i][0] - points[i-1][0]);
+                int y = abs(points[i][1] - points[i-1][1]);
+                cnt += max(x, y);
         }
-        
-        
-        
-        
         return cnt;
-        
     }
 };
