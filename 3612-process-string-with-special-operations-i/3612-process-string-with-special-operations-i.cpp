56ad0c36090464f0590311472a47ee6c355b770f
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-13 10:21:08 +0530

    Time: 5 ms (44.18%), Space: 39.8 MB (68.49%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -1,66 +1,19 @@
 class Solution {
 public:
     string processStr(string s) {
-        // string ans = "";
-        // for (auto i : s) {
-        //     if (isalpha(i)) {
-        //         ans += i;
-        //     } else if (i == '*') {
-        //         if (!ans.empty()) {
-        //             ans.pop_back();
-        //         }
-        //     } else if (i == '#') {
-        //         ans += ans;
-        //     } else if (i == '%') {
-        //         reverse(ans.begin(), ans.end());
-        //     }
-        // }
-        // return ans;
-
-        stack<char> st;
-        for (int i = 0; i < s.size(); i++) {
-
-            if (s[i] == '*') {
-                if (!st.empty())
-                    st.pop();
-            } else if (s[i] == '#') {
-                stack<char> temp;
-                vector<char> seq;
-                while (!st.empty()) {
-                    seq.push_back(st.top());
-                    temp.push(st.top());
-                    st.pop();
-                }
-
-                while (!temp.empty()) {
-                    st.push(temp.top());
-                    temp.pop();
-                }
-
-
-                for (int j = seq.size() - 1; j >= 0; j--) {
-                    st.push(seq[j]);
-                }
-            } else if (s[i] == '%') {
-                stack<char> temp;
-                while (!st.empty()) {
-                    temp.push(st.top());
-                    st.pop();
-                }
-
-                st = temp;
-            } else {
-                st.push(s[i]);
+        string ans = "";
+        for(int i=0;i<s.size();i++){
+            if(isalpha(s[i])){
+                ans += s[i];
+            }else if(s[i] == '*'){
+                if(!ans.empty()) ans.pop_back();
+            }else if(s[i] =='#'){
+                ans += ans;
+            }else if(s[i] == '%'){
+                reverse(begin(ans), end(ans));
             }
         }
 
-        string ans = "";
-        while (!st.empty()) {
-            ans += st.top();
-            st.pop();
-        }
-
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
