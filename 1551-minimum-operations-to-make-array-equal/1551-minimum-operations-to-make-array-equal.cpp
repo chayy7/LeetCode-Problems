ed32d1ea9c38d09b03471aaeece48619da408bf1
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-13 21:54:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (18.44%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int minOperations(int n) {
-        vector<int> vals(n);
-        for(int i=0;i<n;i++){
-            vals[i] = (2 * i) + 1;
-        }
-        int sum = 0;
-        for(auto i : vals){
-            sum += i;
-        }
-        int target = sum/n;
-        int ans = 0;
-        for(auto i : vals){
-            ans += abs(target - i);
-        }
+        // vector<int> vals(n);
+        // for(int i=0;i<n;i++){
+        //     vals[i] = (2 * i) + 1;
+        // }
+        // int sum = 0;
+        // for(auto i : vals){
+        //     sum += i;
+        // }
+        // int target = sum/n;
+        // int ans = 0;
+        // for(auto i : vals){
+        //     ans += abs(target - i);
+        // }
         
-        return ans/2; 
-        
+        return n*n/4; 
     }
 };
