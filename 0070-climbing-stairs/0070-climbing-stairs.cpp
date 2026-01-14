a0f2b4f339d02beba6ed8b6dc177fca1b5ac74d0
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-14 09:35:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (16.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    int fibb(vector<int>& dp,int n){
-        
-        if(n <= 1){
-            return n;
-        }
+    int fib(int n, vector<int> &dp){
+        if(n <= 1) return 1;
 
-        for(int i = 2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+        if(dp[n] != -1) return dp[n];
 
-        return dp[n];
+        return dp[n] = fib(n-1,dp) + fib(n-2,dp);
     }
     int climbStairs(int n) {
-        if(n <= 1){
-            return n;
-        }
-        vector<int> dp(n+1,1);
-        return fibb(dp,n);
+        vector<int> dp(n+1, -1);
+        return fib(n, dp);
     }
 };
