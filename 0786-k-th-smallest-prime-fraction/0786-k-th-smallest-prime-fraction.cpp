3c70e166d46473c558df4fc696d881eb2160769e
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-15 23:17:44 +0530

    Time: 1360 ms (5.07%), Space: 226.7 MB (9.73%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        map<double, pair<int,int>> mp;
-
-        int n = arr.size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                double val = (double)arr[i] / arr[j];
-                mp[val] = {arr[i], arr[j]};
+        map<double,pair<int,int>> mpp;
+        for(int i =0;i<arr.size();i++){
+            for(int j=i;j<arr.size();j++){
+                double c = (double)arr[i]/arr[j];
+                mpp[c] = {arr[i], arr[j]};
             }
         }
 
         int cnt = 0;
-        for (auto &it : mp) {
+        for(auto i:mpp){
             cnt++;
-            if (cnt == k) {
-                return {it.second.first, it.second.second};
+            if(cnt == k){
+                return {i.second.first, i.second.second};
             }
         }
 
