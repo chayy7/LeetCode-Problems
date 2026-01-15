34f2a4bba68498c0515b50906e7e4d4322ca56d0
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-15 20:37:08 +0530

    Time: 64 ms (5.38%), Space: 53.9 MB (5.32%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void hlpr(vector<int>& nums1, vector<int>& nums2, vector<vector<int>> &ans){
-        vector<int> lvl;
-        for(int i=0;i<nums1.size();i++){
-            if(find(nums2.begin(), nums2.end(), nums1[i]) == nums2.end()){
-                lvl.push_back(nums1[i]);
+    void setDiff(set<int> s1, set<int> s2, vector<int> &only1){
+        for(auto i:s1){
+            if(s2.find(i) == s2.end()){
+                only1.push_back(i);
             }
         }
-        ans.push_back(lvl);
     }
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         set<int> s1(nums1.begin(), nums1.end());
         set<int> s2(nums2.begin(), nums2.end());
-        vector<int> numss1(s1.begin(), s1.end());
-        vector<int> numss2(s2.begin(), s2.end());
-
-        vector<vector<int>> ans;
-        hlpr(numss1, numss2, ans);
-        hlpr(numss2, numss1, ans);
-
-        return ans;
+        vector<int> only1;
+        vector<int> only2;
+        setDiff(s1, s2, only1);
+        setDiff(s2, s1, only2);
+        return {only1, only2};
+        
     }
 };
