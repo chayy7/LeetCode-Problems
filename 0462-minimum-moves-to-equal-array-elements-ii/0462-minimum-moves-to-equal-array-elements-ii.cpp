d656774f6f48fe4b6c96f973cfeaaf006151d600
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-16 22:07:37 +0530

    Time: 0 ms (100%), Space: 15.7 MB (50.16%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,18 +3,12 @@ public:
     int minMoves2(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        int sum = 0;
-        for(auto i:nums){
-            sum += i;
+        int mid = nums[n/2];
+        int moves = 0;
+        for(int i: nums){
+            int dif = abs(mid - i);
+            moves += dif;
         }
-        
-
-        int ans = 0;
-        int median = nums[n/2];
-        for(auto i:nums){
-            // cout << median- i<< endl;
-            ans += abs(median- i);
-        }
-        return ans;
-    }
+        return moves;
+    } 
 };
