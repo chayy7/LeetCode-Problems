b455e1e517cb3753868ced0f42cbc38b8f746234
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-16 21:37:28 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.7%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
-    
+    int get_number(char ch){
+        return ('z' - ch + 1);
+    }
     int reverseDegree(string s) {
         int sum_pruduct = 0;
         for(int i=0;i<s.size();i++){
             int idx = i+1;
-            int alpha_number = 'z' - s[i] + 1;
+            int alpha_number = get_number(s[i]);
             int product = idx * alpha_number;
             sum_pruduct += product;
         }
