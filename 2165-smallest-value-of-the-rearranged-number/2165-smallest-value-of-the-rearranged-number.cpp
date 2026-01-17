7031e7e1412235d740c5642cbfc7d12def56326b
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-17 20:46:22 +0530

    Time: 3 ms (9.59%), Space: 11.5 MB (6.64%) - LeetHub

---
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    long long smallestNumber(long long num) {
+        if (num == 0) return 0;
+        long long org = num;
+        vector<string> v;
+        long long numm = llabs(num);
+        while(numm > 0){
+                int rem = numm%10;
+                v.push_back(to_string(rem));
+                numm /= 10;
+        }
+        if(org > 0){
+            
+
+            sort(v.begin(), v.end(), [](string &a, string &b){
+                return a+b < b+a;
+            });
+
+            if(v[0] == "0"){
+                for(int i=1;i< v.size();i++){
+                    if(v[i] != "0"){
+                        swap(v[0], v[i]);
+                        break;
+                    }
+                }
+            }
+        }else{
+            
+            sort(v.begin(), v.end(), [](string &a, string &b){
+                return a+b > b+a;
+            });
+        }
+
+        string s = "";
+        for(auto i:v){
+            s += i;
+        }
+
+        long long ans = stoll(s);
+
+        return org > 0? ans : -ans;
+
+
+
+
+
+
+    }
+};
