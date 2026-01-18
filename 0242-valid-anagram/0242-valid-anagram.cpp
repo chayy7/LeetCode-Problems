d1c9890611b8f7b35b7a24d0a61547b1384efc6d
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-18 16:12:27 +0530

    Time: 3 ms (47.92%), Space: 9.7 MB (47.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+
         if(s.size() != t.size()) return false;
+
         map<char,int> mpp;
-        map<char,int> mpp1;
         for(auto i:s){
             mpp[i]++;
         }
+
         for(auto i:t){
-            mpp1[i]++;
+            mpp[i]--;
         }
 
         for(auto i:mpp){
-            cout << i.first << " " << i.second << endl;
+            if(i.second != 0) return false;
         }
-        for(auto i:mpp1){
-            cout << i.first << " " << i.second << endl;
-        }
-
-
-
-        // for(int i=0;i<s.size();i++){
-        //     cout << mpp[s[i]] << mpp1[s[i]] << endl;
-        //     if( mpp[s[i]] != mpp1[s[i]] ) return false;
-        // }
-
-        return mpp==mpp1;
+        return true;
     }
 };
