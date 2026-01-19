dcc43fbacdb990d78d9bd3278aa219001e21e9bf
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-19 13:03:23 +0530

    Time: 0 ms (100%), Space: 10.4 MB (52.6%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        vector<int> idx;
-        vector<int> vowl;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
-                s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||
-                s[i] == 'O' || s[i] == 'U') {
-                idx.push_back(i);
-                vowl.push_back(s[i]);
+        string v="";
+        // for(auto &i:s){
+        //     if(i == 'A' || i == 'E' || i == 'I' ||  i == 'O' || i == 'U' || i == 'a' || i == 'e' || i == 'i' ||  i == 'o' || i == 'u'){
+                
+        //         v+=i;
+        //         i= '#';
+        //     }
+        // }
+        // cout << v;
+
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||  s[i] == 'O' || s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||  s[i] == 'o' || s[i] == 'u'){
+                v += s[i];
+                s[i] = '%';
             }
         }
-        reverse(vowl.begin(), vowl.end());
-        for (int i = 0; i < idx.size(); i++) {
-            s[idx[i]] = vowl[i];
-        }
+        reverse(v.begin(), v.end());
+        cout << v;
 
+        int j=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '%'){
+                s[i] = v[j++];
+            }
+        }
         return s;
     }
 };
