239a2826cfda56d1c1fd92abd148b27d5c9607b3
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-19 13:04:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     string reverseVowels(string s) {
         string v="";
-        // for(auto &i:s){
-        //     if(i == 'A' || i == 'E' || i == 'I' ||  i == 'O' || i == 'U' || i == 'a' || i == 'e' || i == 'i' ||  i == 'o' || i == 'u'){
+        for(auto &i:s){
+            if(i == 'A' || i == 'E' || i == 'I' ||  i == 'O' || i == 'U' || i == 'a' || i == 'e' || i == 'i' ||  i == 'o' || i == 'u'){
                 
-        //         v+=i;
-        //         i= '#';
-        //     }
-        // }
+                v+=i;
+                i= '#';
+            }
+        }
         // cout << v;
 
 
-        for(int i=0;i<s.size();i++){
-            if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||  s[i] == 'O' || s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||  s[i] == 'o' || s[i] == 'u'){
-                v += s[i];
-                s[i] = '%';
-            }
-        }
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||  s[i] == 'O' || s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||  s[i] == 'o' || s[i] == 'u'){
+        //         v += s[i];
+        //         s[i] = '%';
+        //     }
+        // }
         reverse(v.begin(), v.end());
         cout << v;
 
         int j=0;
         for(int i=0;i<s.size();i++){
-            if(s[i] == '%'){
+            if(s[i] == '#'){
                 s[i] = v[j++];
             }
         }
