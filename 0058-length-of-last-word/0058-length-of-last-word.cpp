bf8e6775f0592fa88d641e6f1f873cfdb54e4b07
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-20 09:55:45 +0530

    Time: 0 ms (100%), Space: 10.2 MB (5.71%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        reverse(s.begin(),s.end());
-        cout << s;
-        stack<char> st;
-        for(int i=0;i<s.size();i++){
-            if(st.empty() && s[i] == ' '){
-                continue;
-            }else if(s[i] != ' '){
-                st.push(s[i]);
-            }else if(!st.empty() && s[i] == ' '){
-                return st.size();
-            }
+        stringstream ss(s);
+
+        string word;
+        vector<string >v;
+        while (ss >> word) {
+            v.push_back(word);
         }
 
-        return st.size();
+
+
+        return v[v.size()- 1].size();
     }
 };
