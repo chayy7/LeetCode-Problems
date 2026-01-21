85084a05c606ef912bd7b45cf0ec1693ab9fba42
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-21 10:02:12 +0530

    Time: 0 ms (100%), Space: 11.2 MB (56.25%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -1,45 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-
-        if(!head ) return head;
-        ListNode* temp=head;
         vector<int> v;
+        ListNode* temp= head;
         while(temp){
             v.push_back(temp->val);
-            temp=temp->next;
+            temp= temp->next;
         }
 
-        int i = 0;
-        int j=i+1;
-        while(i < v.size() && j < v.size()){
-            swap(v[i],v[j]);
-            i+=2;
-            j+=2;
-
-        }
-        for(auto i:v){
-            cout << i << " ";
-        }
         temp=head;
-
-        int c= 0;
-        while(temp){
-            temp->val=v[c++];
-            temp=temp->next;
+        int l= 0, r= 1;
+        while(r < v.size()){
+            swap(v[l], v[r]);
+            l+=2, r+=2;
         }
 
+        int i=0;
+        
+        while(temp){
+            temp->val = v[i++];
+            temp=temp->next;
+        }
 
         return head;
+
+
     }
 };
