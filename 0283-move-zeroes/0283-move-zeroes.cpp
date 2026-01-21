c961370c46b2ee086e7110c21630f3f773359d30
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-21 20:40:03 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& arr) {
-        int idx = 0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i] != 0){
-                swap(arr[i],arr[idx]);
-                idx++;
+    void moveZeroes(vector<int>& nums) {
+        int l=0, r= 1;
+        while(r < nums.size()){
+            if(nums[l] == 0 && nums[r] != 0){
+                swap(nums[l] , nums[r]);
+                l++, r++;
+            }else if (nums[l] != 0) {
+                l++;
+                if (l == r) r++;
+            }else if(nums[r] == 0){
+                r++;
             }
         }
     }
