b9700e756cd032a37574e9b3ac56009312db052d
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-23 20:38:02 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.73%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,46 +1,37 @@
 class Solution {
-public:
+public: // chan
     bool backspaceCompare(string s, string t) {
-        stack<char> s1;
+        stack<char> st1;
+        stack<char> st2;
         for(int i=0;i<s.size();i++){
-            if(s1.empty() && s[i] != '#'){
-                s1.push(s[i]);
-            }else if(s[i] == '#'){
-                if(!s1.empty()){
-                    s1.pop();
+            if(s[i] == '#'){
+                if(!st1.empty()){
+                    st1.pop();
                 }
             }else{
-                s1.push(s[i]);
+                st1.push(s[i]);
             }
-            
         }
-        stack<char> s2;
         for(int i=0;i<t.size();i++){
-            if(s2.empty() && t[i] != '#'){
-                s2.push(t[i]);
-            }else if(t[i] == '#'){
-                if(!s2.empty()){
-                    s2.pop();
+            if(t[i] == '#'){
+                if(!st2.empty()){
+                    st2.pop();
                 }
             }else{
-                s2.push(t[i]);
+                st2.push(t[i]);
             }
-            
         }
-
-        if(s1.size() != s2.size()) return false;
-
-        // int i=0;
-        while(s1.size()){
-            if(s1.top() != s2.top()){
-                return false;
-            }
-
-            s1.pop();
-            s2.pop();
-            // i++;
+        string s_str = "";
+        string t_str = "";
+        while(!st1.empty()){
+            s_str += st1.top();
+            st1.pop();
         }
-
-        return true;
+        while(!st2.empty()){
+            t_str += st2.top();
+            st2.pop();
+        }
+        
+        return s_str == t_str;
     }
 };
