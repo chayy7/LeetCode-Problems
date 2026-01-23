2ff4521c1ef06940c2c0c9ec73dd1a0c36b17034
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-23 09:23:30 +0530

    Time: 0 ms (100%), Space: 8.1 MB (15.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    int fibb(int n, vector<int>& dp){
+    int func(int n, vector<int>& dp){
         if(n <= 1){
-            dp[n]=n;
+            dp[n] = n;
             return n;
         }
-
         if(dp[n] != -1) return dp[n];
 
-        return dp[n] = fibb(n-1, dp)+ fibb(n-2, dp);
+        return dp[n] = func(n-1, dp) + func(n-2, dp);
     }
     int fib(int n) {
         vector<int> dp(n+1, -1);
-        fibb(n, dp);
-        for(auto i: dp){
-            cout <<  i <<" ";
-        }
-
-        return fibb(n,dp);
-
+        return func(n , dp);
     }
 };
