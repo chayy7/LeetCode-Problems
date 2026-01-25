307865806ee321094b5712303afbe7fb6a577859
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-25 10:02:49 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.9%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        string org = s;
-        for(int i=0;i<s.size();i++){
-            reverse(s.begin(), s.begin()+i);
-            reverse(s.begin()+i, s.end());
-            reverse(s.begin(), s.end());
-            if(s == goal){
-                return true;
-            }
-            s = org;
-        }
-
-        return false;
-        
+        if(s.size() != goal.size()) return false;
+        s = s+s;
+        return s.find(goal) != string::npos;
     }
 };
