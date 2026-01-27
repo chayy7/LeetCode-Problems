ea0f5edde7d36bdbfc0e6aea0050e595b89de2f1
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-27 18:18:18 +0530

    Time: 0 ms (100%), Space: 15.1 MB (15.92%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int row = mat.size();
-        int col = mat[0].size();
+        int n=mat.size();
         int sum = 0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(i == j){
-                    sum += mat[i][j];
-                }else if(i + j == row -1){
+        for(int i=0;i<n;i++){
+            for(int j= 0;j < n;j++){
+                if(i == j || ((i+j) % (n-1)) == 0){
                     sum += mat[i][j];
                 }
             }
         }
+
         return sum;
-        
     }
 };
-
-// y not dhurandhar? 
