7fee2769a6818ba292da4ff094d0381ac5fef457
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-27 22:05:52 +0530

    Time: 12 ms (5.49%), Space: 47 MB (5.5%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> p(n);
-        int prod = 1;
-        for(int i=0;i<n;i++){
-            p[i] = prod * nums[i];
-            prod = prod * nums[i];
+        int pref_prod = 1;
+        vector<int> pref;
+        for (int i = 0; i < nums.size(); i++) {
+            pref.push_back(pref_prod * nums[i]);
+            pref_prod = pref_prod * nums[i];
         }
-        vector<int> s(n);
-        int pro = 1;
-        for(int i=n-1;i>=0;i--){
-            s[i] = pro*nums[i];
-            pro *= nums[i];
+        int suf_prod = 1;
+        vector<int> suf;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            suf.push_back(suf_prod * nums[i]);
+            suf_prod = suf_prod * nums[i];
         }
+        reverse(suf.begin(), suf.end());
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == 0) { 
+                ans.push_back(suf[i + 1]);
+            } else if (i == nums.size() - 1) {
+                ans.push_back(pref[i - 1]);
+            } else {
 
-        for(auto i:p){
-            cout << i << " ";
-        }
-        for(auto i:s){
-            cout << i << " ";
-        }
-        vector<int> v;
-
-
-        for(int i=0;i<n;i++){
-            if(i == 0){
-                v.push_back(s[i+1]);
-            }else if(i == n-1){
-                v.push_back(p[i-1]);
-
-            }else{
-                v.push_back(p[i-1] * s[i+1]);
+                ans.push_back(pref[i - 1] * suf[i + 1]); 
             }
         }
 
-        return v;
+       
+
+        return ans;
     }
 };
