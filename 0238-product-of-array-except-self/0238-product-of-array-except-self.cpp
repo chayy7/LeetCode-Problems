dafc1ef3125d1837195cffd3682d58acdb78b4fd
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-27 16:22:30 +0530

    Time: 861 ms (5.49%), Space: 43.8 MB (5.5%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int p=1;
-        int z=0;
-        for(auto i:nums){
+        int n=nums.size();
+        vector<int> p(n);
+        int prod = 1;
+        for(int i=0;i<n;i++){
+            p[i] = prod * nums[i];
+            prod = prod * nums[i];
+        }
+        vector<int> s(n);
+        int pro = 1;
+        for(int i=n-1;i>=0;i--){
+            s[i] = pro*nums[i];
+            pro *= nums[i];
+        }
+
+        for(auto i:p){
+            cout << i << " ";
+        }
+        for(auto i:s){
+            cout << i << " ";
+        }
+        vector<int> v;
+
+
+        for(int i=0;i<n;i++){
             if(i == 0){
-                z++;
-                continue;
-            }
-            p *= i;
-        }      
+                v.push_back(s[i+1]);
+            }else if(i == n-1){
+                v.push_back(p[i-1]);
 
-        
-        vector<int> ans;
-        if(z > 1){
-            for (auto i:nums) {
-                ans.push_back(0);
-            }
-            return ans;
-        }
-        if(z == 1){
-            for(auto i:nums){
-                if(i == 0){
-                    ans.push_back(p);
-                }else{
-                    ans.push_back(0);
-                }
-            }
-        }else if(z == 0){
-            for(auto i:nums){
-                    ans.push_back(p/i);
-
+            }else{
+                v.push_back(p[i-1] * s[i+1]);
             }
         }
 
-        return ans;
-        
+        return v;
     }
 };
