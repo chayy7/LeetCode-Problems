7df403fe54f962b2bfa27683cc83bf6326548b6c
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-27 18:29:12 +0530

    Time: 0 ms (100%), Space: 15.1 MB (34.96%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> transpose(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-
-        vector<vector<int>> v(m, vector<int>(n));
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                v[j][i] = mat[i][j];
+    vector<vector<int>> transpose(vector<vector<int>>& m) {
+        vector<vector<int>> res(m[0].size(), vector<int>(m.size()));
+        for(int i=0;i<m.size();i++){
+            for(int j=0;j<m[0].size();j++){
+                res[j][i] = m[i][j];
             }
         }
-
-        for(auto i:v){
-            for(auto j:i){
-                cout << j << " ";
-            }
-
-            // cout << i << " ";
-        }
-
-        return v;
+        return res;
     }
 };
