047d39a91c12498f9bfafbb1cb755c64e93918e5
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-27 22:26:28 +0530

    Time: 912 ms (5.14%), Space: 187.7 MB (5.14%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -2,48 +2,49 @@ class Solution {
 public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         vector<long long> nums;
-        for(auto i:grid){
-            for(auto j:i){
+        for (auto i : grid) {
+            for (auto j : i) {
                 nums.push_back(j);
             }
         }
-        int n = nums.size();
-        vector<long long> p(n);
-        int prod = 1;
-        for(int i=0;i<n;i++){
-            p[i] = (prod * nums[i]) % 12345;
-            prod = (prod * nums[i]) % 12345;
+
+        long long pref_prod = 1;
+        vector<long long> pref;
+        for (long long i = 0; i < nums.size(); i++) {
+            pref.push_back((pref_prod * nums[i]) % 12345);
+            pref_prod = (pref_prod * nums[i]) % 12345;
         }
-        vector<long long> s(n);
-        int pro = 1;
-        for(int i=n-1;i>=0;i--){
-            s[i] = (pro*nums[i]) % 12345;
-            pro = (pro *nums[i]) % 12345;
+        long long suf_prod = 1;
+        vector<long long> suf;
+        for (long long i = nums.size() - 1; i >= 0; i--) {
+            suf.push_back(((suf_prod * nums[i])) % 12345);
+            suf_prod = (suf_prod * nums[i] )% 12345;
         }
+        reverse(suf.begin(), suf.end());
+        vector<long long> ans;
+        for (long long i = 0; i < nums.size(); i++) {
+            if (i == 0) {
+                ans.push_back(suf[i + 1] % 12345);
+            } else if (i == nums.size() - 1) {
+                ans.push_back(pref[i - 1] % 12345);
+            } else {
 
-        
-        vector<long long> v;
-
-
-        for(int i=0;i<n;i++){
-            if(i == 0){
-                v.push_back(s[i+1] % 12345);
-            }else if(i == n-1){
-                v.push_back(p[i-1] % 12345);
-
-            }else{
-                v.push_back((p[i-1] * s[i+1]) % 12345);
+                ans.push_back((pref[i - 1] * suf[i + 1]) % 12345);
             }
         }
 
-        long long cnt = 0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                grid[i][j] = (v[cnt++]) % 12345;
+        for (auto i : ans) {
+            cout << i << " ";
+        }
+        long long row = grid.size();
+        long long col = grid[0].size();
+        long long iterator = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                grid[i][j] = ans[iterator++] % 12345;
             }
         }
 
         return grid;
-
     }
 };
