43f0e649120f69b33415977498b7766baef70b37
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-28 20:52:37 +0530

    Time: 0 ms (100%), Space: 11.3 MB (78.7%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> v(heights);
+        sort(v.begin(),v.end());
+        int cnt = 0;
+
+
+        for(int i = 0;i<v.size();i++){
+            if(v[i] != heights[i]){
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+};
