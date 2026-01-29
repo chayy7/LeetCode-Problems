00a9b0be86b6da4ec0c589ae549a28e0e70b7159
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-29 21:50:45 +0530

    Time: 5 ms (58.35%), Space: 11.9 MB (70.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int n=s.size();
-
-        map<char,int> mpp;
-        mpp['I']=1;
-        mpp['V']=5;
-        mpp['X']=10;
-        mpp['L']=50;
-        mpp['C']=100;
-        mpp['D']=500;
-        mpp['M']=1000;
-        // int I = 1;
-        // int V = 5;
-        // int X = 10;
-        // int L = 50;
-        // int C = 100;
-        // int D = 500;
-        // int M = 1000;
-
-        int sum =0;
-        for(int i=0;i<n-1;i++){
-            if(mpp[s[i]] < mpp[s[i+1]]){
-                sum -= mpp[s[i]];
-            }else{
-
-                sum += mpp[s[i]];
+        vector<pair<int, string>>roman = {
+            {1000, "M"},
+            {500, "D"},
+            {100, "C"},
+            {50, "L"},
+            {10, "X"},
+            {5,"V"},
+            {1, "I"},
+        };
+        int ans = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i] == 'C' && s[i+1] == 'M'){
+                ans += 900; i++; continue;
+            }
+            if(s[i] == 'C' && s[i+1] == 'D'){
+                ans += 400; i++; continue;
+            }
+            if(s[i] == 'X' && s[i+1] == 'C'){
+                ans += 90; i++; continue;
+            }
+            if(s[i] == 'X' && s[i+1] == 'L'){
+                ans += 40; i++; continue;
+            }
+            if(s[i] == 'I' && s[i+1] == 'X'){
+                ans += 9; i++; continue;
+            }
+            if(s[i] == 'I' && s[i+1] == 'V'){
+                ans += 4; i++; continue;
+            }
+            for(auto k: roman){
+                if(k.second[0] == s[i]){
+                    ans += k.first;
+                    break;
+                }
             }
         }
-
-        sum += mpp[s[n-1]];
-
-        return sum;
-
+        return ans;
         
     }
 };
