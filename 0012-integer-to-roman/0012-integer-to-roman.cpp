921619eb8441eadad84ab6a77c2c6f979afdc2f2
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-29 21:21:20 +0530

    Time: 111 ms (5.86%), Space: 12.8 MB (9.48%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        map<int,string,greater<int>> mpp;
-        mpp[1000]="M";
-        mpp[900]="CM";
-        mpp[500]="D";
-        mpp[400]="CD";
-        mpp[100]="C";
-        mpp[90]="XC";
-        mpp[50]="L";
-        mpp[40]="XL";
-        mpp[10]="X";
-        mpp[9]="IX";
-        mpp[5]="V";
-        mpp[4]="IV";
-        mpp[1]="I";
-
-        string ans="";
-        for(auto i:mpp){
+        vector<pair<int, string>>roman = {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5,"V"},
+            {4,"IV"},
+            {1, "I"},
+        };
+        for(auto i: roman){
+            cout << i.first << " ";
+        }
+        string  ans = "";
+        for(auto i: roman){
             while(num >= i.first){
-                num -= i.first;
                 ans += i.second;
+                num -= i.first;
             }
         }
-
         return ans;
     }
 };
