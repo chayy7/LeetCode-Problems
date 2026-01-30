dec631a8f1f2bb027f7dcf0eaaa2e37f2b20bc2e
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-30 06:53:45 +0530

    Time: 0 ms (100%), Space: 8.3 MB (53.69%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -3,20 +3,19 @@ public:
     int balancedStringSplit(string s) {
         int n=s.size();
 
-        int l =0, r= 0;
+        int cnt = 0;
         int ans = 0;
         for(int i=0;i<n;i++){
             if(s[i] == 'R'){
-                r++;
+                cnt++;
             }else if(s[i] == 'L'){
-                l++;
+                cnt--;
             }
 
-            if(l == r){
+            if(cnt == 0){
                 ans++;
             }
         }
-
         return ans;
     }
 };
