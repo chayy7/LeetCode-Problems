0df14a6eaabd25b756034d2f84d6d261959fa284
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-01-31 22:26:15 +0530

    Time: 40 ms (73.94%), Space: 53.4 MB (75.39%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    let cnt = n-1;
+    return function() {
+        cnt+=1;
+        return cnt;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
