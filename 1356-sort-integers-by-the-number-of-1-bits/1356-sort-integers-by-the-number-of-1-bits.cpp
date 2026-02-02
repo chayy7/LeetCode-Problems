347013ded5d5f262f7a65720c92efb79387488ce
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-02 22:03:32 +0530

    Time: 3 ms (46.16%), Space: 16 MB (6.05%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,24 +1,22 @@
-bool comp(pair<int, int>&a, pair<int, int>&b){
-    if(a.second == b.second)
+bool comp(pair<int, int>& a, pair<int, int>& b) {
+    if (a.second == b.second)
         return a.first < b.first;
     return a.second < b.second;
 }
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        multimap<int,int> bitCount;
-        for(auto i: arr){
-            bitCount.insert({i,__builtin_popcount(i)});
-        }
         vector<int> ans;
-        
-         vector<pair<int,int>> sort_bitCout(bitCount.begin(), bitCount.end());
-         sort(sort_bitCout.begin(), sort_bitCout.end(), comp);
-         
-         for(auto i:sort_bitCout){
-            ans.push_back(i.first);
-         }
 
+        vector<pair<int, int>> bitCout;
+        for(auto i: arr){
+            bitCout.push_back({i, __builtin_popcount(i)});
+        }
+        sort(bitCout.begin(), bitCout.end(), comp);
+
+        for (auto i : bitCout) {
+            ans.push_back(i.first);
+        }
 
         return ans;
     }
