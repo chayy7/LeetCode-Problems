75943911fc2b4b0fb1a4574071596be9652c1212
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-02 22:35:47 +0530

    Time: 0 ms (100%), Space: 12.4 MB (78.21%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int countElements(vector<int>& nums) {
+        int count = 0;
+        int max_elem = *max_element(nums.begin(), nums.end());
+        int min_elem = *min_element(nums.begin(), nums.end());
         sort(nums.begin(), nums.end());
-
-        int mn = nums[0];
-        int mx = nums[nums.size()-1];
-        int cnt =0;
-        for(auto i:nums){
-            if(i > mn && i<mx) cnt++;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] > min_elem && nums[i] < max_elem){
+                count++;
+            }
         }
-
-        return cnt;
+        return count;
     }
 };
