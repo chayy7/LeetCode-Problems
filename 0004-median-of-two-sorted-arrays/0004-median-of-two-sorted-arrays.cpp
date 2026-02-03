4e427e2763d6cb1647636f45438e737b1bc37594
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-03 21:45:13 +0530

    Time: 3 ms (29.48%), Space: 96 MB (18.31%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-        // mona shall i say u one plan? for both of us? say
-        // we ll prepare for apti, ey regularly, okaa 1 hr, ok
-        // in clg , not here, here only dsa...after lunch , we ll just bunk those classes
-        // say smthg
         vector<int> mergedArr;
         int st1 = 0, st2 = 0;
         while(st1 < nums1.size() && st2 < nums2.size()){
             if(nums1[st1] <= nums2[st2]){
                 mergedArr.push_back(nums1[st1]);
                 st1++;
-            }else{ // good observation
+            }else{ 
                 mergedArr.push_back(nums2[st2]);
                 st2++;
             }
@@ -26,7 +21,7 @@ public:
         
         double med = 0.0;
         if(mergedArr.size() % 2 == 0){
-            med = (mergedArr[mergedArr.size() / 2] + mergedArr[mergedArr.size() / 2 - 1])/2.0; 
+            med = (mergedArr[mergedArr.size() / 2] + mergedArr[mergedArr.size() / 2 - 1])/2.0;
         }else{
             med = mergedArr[mergedArr.size() / 2];
         }
