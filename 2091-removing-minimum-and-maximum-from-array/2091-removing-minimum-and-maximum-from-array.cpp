bf237b1e5e3e94f6ddf5e07c21582677840dbd00
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-09 22:23:33 +0530

    Time: 7 ms (11.49%), Space: 91.9 MB (86.21%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int n=nums.size();
-        int mn = *min_element(nums.begin(), nums.end());
-        int mx = *max_element(nums.begin(), nums.end());
-
-        int mn_idx = -1;
-        int mx_idx = -1;
-        for(int i=0;i<n;i++){
-            if(nums[i] == mn) mn_idx = i;
-            if(nums[i] == mx) mx_idx = i;
+        int len = nums.size();
+        int max_elem = INT_MIN;
+        int max_idx = 0;
+        int min_elem = INT_MAX;
+        int min_idx = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] > max_elem){
+                max_elem = nums[i];
+                max_idx = i;
+            }
+            if(nums[i] < min_elem){
+                min_elem = nums[i];
+                min_idx = i;
+            }
         }
-        int mn_idx_b = n- mn_idx ;
-        int mx_idx_b = n-mx_idx;
-
-        // cout << mn_idx << " " << mx_idx << endl;
-        // cout << mn_idx_b << " " << mx_idx_b << endl;
-
-        int ff = max((mn_idx+ 1),(mx_idx+1));
-        int bb = max((mn_idx_b), (mx_idx_b));
-        int fb = min(mn_idx+1 + mx_idx_b, mn_idx_b+1+ mx_idx);
-
-        int ans = min(ff, min(bb, fb));
-        return ans;
+        int front = max(min_idx, max_idx) + 1;
+        int back = len - min(min_idx, max_idx);
+        int mix = (min(min_idx, max_idx) + 1) + (len - max(min_idx, max_idx));
+        return min({front, back, mix});
+        
     }
 };
