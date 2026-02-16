51b8f4b339b8268e4606ef4f8e6dd08ada901654
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-16 22:26:36 +0530

    Time: 25 ms (5.62%), Space: 19.3 MB (56.35%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.py
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -1,25 +1,30 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        xcnt = 0
-        ycnt = 0
-
-        s = []
-        g = []
-
-
-
-        for i in range(len(secret)):
-            if secret[i] == guess[i]:
+        
+        xcnt =0
+        ycnt =0
+        
+        s=[]
+        g=[]
+            
+        l =0
+        r=0
+        while(l < len(secret)):
+            if(secret[l] == guess[r]):
                 xcnt += 1
             else:
-                s.append(secret[i])
-                g.append(guess[i])
-
-
-
-        for ch in g:
-            if ch in s:
-                ycnt += 1
-                s.remove(ch)  
-
-        return f"{xcnt}A{ycnt}B"
+                s.append(secret[l])
+                g.append(guess[l])
+            l+=1
+            r+=1
+            
+        for i in g:
+            if i in s:
+                ycnt+=1
+                s.remove(i)
+        ans=""
+        ans += str(xcnt)
+        ans += "A"
+        ans += str(ycnt)
+        ans += "B"
+        return ans
