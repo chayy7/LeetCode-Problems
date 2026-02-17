ac7e76ab538586c0baa7d4e350b2c3fb23e6abdf
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-17 21:37:41 +0530

    Time: 3 ms (9.95%), Space: 9.7 MB (41.32%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     vector<string> readBinaryWatch(int turnedOn) {
+        if(turnedOn >= 9)return {};
         vector<string> ans;
-        for(int i=0;i<=11;i++){
-            for(int j=0;j<=59;j++){
-                if(__builtin_popcount(i)+__builtin_popcount(j) == turnedOn){
-                    string time = to_string(i)+":";
-                    if(j < 10){
-                        time += "0";
+        for(int hr=0;hr<12;hr++){ 
+            int hr_bit_count = __builtin_popcount(hr);
+            for(int min=0;min<60;min++){ 
+                int min_bit_count = __builtin_popcount(min);
+                if((hr_bit_count + min_bit_count) == (turnedOn)){
+                    
+                    string time = "";
+                    if(min <= 9){
+                        time = to_string(hr) + ":0" + to_string(min);
+                    }else{
+                        
+                        time = to_string(hr) + ":" + to_string(min);
                     }
-                    time += to_string(j);
                     ans.push_back(time);
                 }
             }
         }
         return ans;
+            
     }
 };
