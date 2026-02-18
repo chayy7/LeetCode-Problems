2d0a4baa05880196103cc0a002855b675dee225f
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-18 14:46:55 +0530

    Time: 0 ms (100%), Space: 8.1 MB (5.31%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -11,11 +11,12 @@ public:
     }
     bool isStrictlyPalindromic(int n) {
         for(int i=2;i<= n-2;i++){
-            string s = bitset<32>(i).to_string();
-            string ss = s.substr(s.find('1'));
-            string revss = baseconv(n, i);
+            
+            string ss = baseconv(n, i);
+            string revss =ss;
+            reverse(revss.begin(), revss.end());
             if(ss != revss) return false;
-            cout << baseconv(n,i ) << endl;
+            // cout << baseconv(n,i ) << endl;
         }
 
         return true;
