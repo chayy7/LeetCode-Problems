56befe79996e5bcde90a9d3caf6d56f9a1fae189
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-18 22:25:37 +0530

    Time: 0 ms (100%), Space: 7.7 MB (62.63%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        
-        string bit = bitset<16>(n).to_string();
-        
+        /* string bit = bitset<16>(n).to_string();
         string new_bit = bit.substr(bit.find('1'));
-        
-        
-        cout << new_bit;
-        
-        for(int i=1;i<new_bit.size();i++){
+        cout << new_bit;        
+        for(int i=1;i<bit.size();i++){
             if(new_bit[i-1] == new_bit[i]) return false;
         }
+        return true; */
         
+        
+        
+        int prev = -1;
+        while(n > 0){
+            int rem = n%2;
+            if(rem == prev){
+                return false;
+            }
+            prev = rem;
+            n /= 2;
+        }
         return true;
     }
 };
