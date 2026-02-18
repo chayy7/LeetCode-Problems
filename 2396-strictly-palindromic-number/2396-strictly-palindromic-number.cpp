b52025fceb62cb54d83dade4aeaf55c338cc9bd3
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-18 14:30:43 +0530

    Time: 0 ms (100%), Space: 7.6 MB (99.56%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
-    string baseCnv(int n, int base){
-        string temp  = "";
-        while(n > 0){
-            int rem = n % base;
-            temp += char('0' - rem);
-            n /= base;
-        }
-        reverse(temp.begin(), temp.end());
-        return temp;
-    }
     bool isStrictlyPalindromic(int n) {
-        for(int i=2;i<=n-2;i++){
-            string str = baseCnv(n, i);
-            string rev_str = str;
-            reverse(rev_str.begin(), rev_str.end());
-            if(str != rev_str){
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 };
