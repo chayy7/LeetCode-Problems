f68b6c2922539e9d788d226615ba3d4b1c6d4971
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-21 10:37:21 +0530

    Time: 0 ms (100%), Space: 8.5 MB (48.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,13 +4,11 @@ public:
         if(n <= 1) return 1;
 
         if(dp[n] != -1) return dp[n];
-            
 
-        return dp[n] = func(n-1, dp) + func(n-2, dp);
+        return dp[n]= func(n-1,dp)+func(n-2,dp);
     }
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
         return func(n, dp);
-
     }
 };
