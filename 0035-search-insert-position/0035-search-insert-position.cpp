63e8129eb4dd416cb0197d314ddcf30977c64a35
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-23 13:03:49 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.34%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,15 +3,17 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int n=nums.size();
 
-        int l=0, r= n-1;
+        int l=0, r=n-1;
         while(l <= r){
-            int mid = (l+r)/2;
-            if(nums[mid] >= target){
+            int mid=l+(r-l)/2;
+            if(target <= nums[mid]){
                 r=mid-1;
-            }else if(nums[mid] < target){
+
+            }else{
                 l=mid+1;
             }
         }
+
         return l;
     }
 };
