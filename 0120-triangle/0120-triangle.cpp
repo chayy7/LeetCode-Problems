386e9085b67dabed166cf537f8ff6ae33c56d97e
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-23 21:35:39 +0530

    Time: 2 ms (16.41%), Space: 13.4 MB (23.59%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int func(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& tri) {
-        int n = tri.size();
-        if (i == n - 1)
-            return tri[i][j];
+    int func(int i, int j, vector<vector<int>>& dp,int n,vector<vector<int>>& tri){
+        if(i == n-1) return tri[i][j];
 
-        if (dp[i][j] != INT_MAX)
-            return dp[i][j];
+        if(dp[i][j] != INT_MAX) return dp[i][j];
 
-        int down = func(i + 1, j, dp, tri);
-        int diag = func(i + 1, j + 1, dp, tri);
-
-        return dp[i][j] = tri[i][j] + min(down, diag);
+        int down=tri[i][j] + func(i+1, j, dp, n, tri);
+        int diag=tri[i][j] + func(i+1, j+1, dp, n, tri);
+        return dp[i][j] = min(down, diag);
     }
-
     int minimumTotal(vector<vector<int>>& tri) {
-        int n = tri.size();
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        int n=tri.size();
 
-        return func(0, 0, dp, tri);
+        int i=0, j=0;
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        return func(i, j, dp,n, tri);
     }
 };
