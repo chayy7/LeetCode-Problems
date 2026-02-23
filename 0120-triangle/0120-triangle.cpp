b9bd3223d15982195bfb08a8cf03017b4c703c2d
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-23 21:20:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int n;
-    int findMin(int i, int j, vector<vector<int>>& t) {
-        if (dp[i][j] != INT_MAX) return dp[i][j];
-        if (i == 0) return dp[0][0] = t[0][0];
-        if (j == 0) return dp[i][j] = t[i][j] + findMin(i-1, j, t);
-        if (i == j) return dp[i][j] = t[i][j] + findMin(i-1,j-1, t);
-        return dp[i][j] = 
-        t[i][j] + min(findMin(i - 1, j, t), findMin(i - 1, j - 1, t));
+    int func(int i, int j, vector<vector<int>>& dp,int n,vector<vector<int>>& tri){
+        if(i == n-1) return tri[n-1][j];
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int down=tri[i][j] + func(i+1, j, dp, n, tri);
+        int diag=tri[i][j] + func(i+1, j+1, dp, n, tri);
+        return dp[i][j] = min(down, diag);
     }
+    int minimumTotal(vector<vector<int>>& tri) {
+        int n=tri.size();
 
-    int minimumTotal(vector<vector<int>>& triangle) {
-        n = triangle.size();
-        dp.assign(n, vector<int>(n, INT_MAX));
-
-        int ans = INT_MAX;  
-        for (int j = 0; j < n; j++) {
-            ans = min(ans, findMin(n - 1, j, triangle));
-        }
-        return ans;
-    }      
+        int i=0, j=0;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return func(i, j, dp,n, tri);
+    }
 };
