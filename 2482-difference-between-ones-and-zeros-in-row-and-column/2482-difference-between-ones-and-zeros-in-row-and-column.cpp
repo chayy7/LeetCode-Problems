5639e9322dec73b97a2948c62d20933c344ffcc8
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-03-07 21:44:24 +0530

    Time: 368 ms (5.07%), Space: 184.7 MB (5.28%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+        map<int, int> row1;
+        map<int, int> row0;
+        map<int, int> col1;
+        map<int, int> col0;
         
-
-        map<int,int> onesRow;
-        map<int,int> onesCol;
-        map<int,int> zeroesRow;
-        map<int,int> zeroesCol;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int row = grid.size();
+        int col = grid[0].size();
+        vector<vector<int>> ans(row, vector<int>(col));
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(grid[i][j] == 1){
-                    onesRow[i]++;
-                    onesCol[j]++;
-                }
+                    row1[i]++;
+                    col1[j]++;
+                }else if(grid[i][j] == 0){
+                    row0[i]++;
+                    col0[j]++;
+                }                
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 0){
-                    zeroesRow[i]++;
-                    zeroesCol[j]++;
-                }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                ans[i][j] = row1[i] + col1[j] - row0[i] - col0[j];
             }
         }
-
-
-        vector<vector<int>> v(n, vector<int>(m));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                v[i][j] = onesRow[i] + onesCol[j] - zeroesRow[i] - zeroesCol[j];
-            }
-        }
-        return v;
+        return ans;
     }
 };
