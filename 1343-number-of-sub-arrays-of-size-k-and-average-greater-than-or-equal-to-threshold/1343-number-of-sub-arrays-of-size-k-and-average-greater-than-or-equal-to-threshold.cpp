ae2035bd32e2a4fe6aab233ecaaf0b53df3afcde
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-03-13 21:44:04 +0530

    Time: 4 ms (16.56%), Space: 59.2 MB (37.11%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
-        if(k == 50000) return 50001;
-        int count = 0;
-        int left = 0, right = k - 1;
-        int n = arr.size() - 1;
+        // better appoach
+        // -------------
         
-        while(right <= n){
-            int avg = 0;
-            int st = left;
-            int sum = 0;
-            while(st <= right){
-                sum += arr[st];
-                st++;
-            }
-            avg = sum / k;
-            if(avg >= threshold){
+        // if(k == 50000) return 50001; 
+        // int count = 0;
+        // int left = 0, right = k - 1;
+        // int n = arr.size() - 1;
+        
+        // while(right <= n){
+        //     int avg = 0;
+        //     int st = left;
+        //     int sum = 0;
+        //     while(st <= right){
+        //         sum += arr[st];
+        //         st++;
+        //     }
+        //     avg = sum / k;
+        //     if(avg >= threshold){
+        //         count++;
+        //     }
+        //     left++;
+        //     right++;
+        // }
+        // return count;
+        // -------------------------------------------------
+        // optimal approach
+        // ----------------
+        
+        int count = 0;
+        int sum = 0;
+        int n = arr.size();
+        for(int i=0;i<k;i++){
+            sum += arr[i];
+        }
+        if(sum / k >= threshold){
+            count++;
+        }
+        for(int i=k;i<n;i++){
+            sum += arr[i];
+            sum -= arr[i-k];
+            if(sum / k >= threshold){
                 count++;
             }
-            left++;
-            right++;
         }
         return count;
-        
     }
 };
