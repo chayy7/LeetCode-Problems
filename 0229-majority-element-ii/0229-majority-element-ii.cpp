301d37976b3001e73a3248d474c7b6b50efbd4fe
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-03-16 22:16:26 +0530

    Time: 8 ms (30.9%), Space: 26.5 MB (15.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-            
-        map<int,int> mpp;
-        for(auto i:nums){
+        // next next
+        vector<int> ans;
+        unordered_map<int, int> mpp;
+        for(int i: nums){
             mpp[i]++;
         }
-        vector<int> ans;
-        for(auto i:mpp){
-            if(i.second > nums.size()/3){
+        int n = nums.size();
+        int critiria = n/3;
+        for(auto &i : mpp){
+            if(i.second > critiria){
                 ans.push_back(i.first);
             }
         }
-        
         return ans;
-        
-             
-        
- 
-        // int n = nums.size();
-        //     vector <int> result;
-        //     int duplicate = n/3;
-        //     for(int i=0;i<n;i++){
-        //         int count = 1;
-        //         for(int j=i+1;j<n;j++){
-        //             if(nums[i]==nums[j]){
-        //                 count++;
-        //             }
-        //         }
-            
-        //         bool isThere = false;
-        //         for(int k=0;k<result.size();k++){
-        //             if(result[k] == nums[i]){
-        //                 isThere = true;
-        //                 break;
-        //             }
-        //         }
-        //         if(count > duplicate && !isThere){
-        //             result.push_back(nums[i]);
-        //         }
-                
-        //     }
-            
-        //    return result; 
     }
 };
