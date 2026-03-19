893621e9c6db72df480d6b231a50050a899a2ad4
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-03-20 00:02:01 +0530

    Time: 0 ms (100%), Space: 24 MB (97.99%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int s = flowerbed.size();
+
+        if(flowerbed[0] == 1){
+            int i = 0;
+            while(i < s && n > 0){
+                
+                if(flowerbed[i] == 0){
+                    
+                    if(i != 0 && flowerbed[i-1] == 1){
+                        i++;
+                        continue;
+                    }
+                    
+                    if(i != s-1 && flowerbed[i+1] == 1){
+                        i++;
+                        continue;
+                    }
+                    
+                    flowerbed[i] = 1;
+                    n--;
+                    i += 2;
+                    continue;
+                }
+                
+                i++;
+            }
+        }
+        
+        else {  
+            if(n > 0 && (s == 1 || flowerbed[1] == 0)){
+                flowerbed[0] = 1;
+                n--;
+            }
+
+            int i = 2; 
+            while(i < s && n > 0){
+                
+                if(flowerbed[i] == 0){
+                    
+                    if(flowerbed[i-1] == 1){
+                        i++;
+                        continue;
+                    }
+                    
+                    if(i != s-1 && flowerbed[i+1] == 1){
+                        i++;
+                        continue;
+                    }
+                    
+                    flowerbed[i] = 1;
+                    n--;
+                    i += 2;
+                    continue;
+                }
+                
+                i++;
+            }
+        }
+
+        return n <= 0;
+    }
+};
