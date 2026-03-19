7484f1be28400f65e66c4f8b5d17fddbb951f204
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-03-19 09:48:42 +0530

    Time: 0 ms (100%), Space: 17.4 MB (88.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
-    int fbs(vector<int>& nums, int target){
-        int n=nums.size();
-
-        int l=0, r=n-1;
-        while(l <= r){
-            int mid= l+(r-l)/2;
-            if(target <= nums[mid]){
-                r=mid-1;
-            }else{
-                l=mid+1;
-            }
-        }
-        return l;
-    }
-    int lbs(vector<int>& nums, int target){
-        int n=nums.size();
-
-        int l=0, r=n-1;
-        while(l <= r){
-            int mid= l+(r-l)/2;
-            if(target < nums[mid]){
-                r=mid-1;
-            }else{
-                l=mid+1;
-            }
-        }
-        return r;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int a= fbs(nums, target);
-        int b= lbs(nums, target);
-
-        if(a > b) return {-1,-1};
-        return {a,b};
+        vector<int> result = {-1, -1};
+        int left = binarySearch(nums, target, true);
+        int right = binarySearch(nums, target, false);
+        result[0] = left;
+        result[1] = right;
+        return result;        
     }
+
+    int binarySearch(vector<int>& nums, int target, bool isSearchingLeft) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int idx = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                idx = mid;
+                if (isSearchingLeft) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        
+        return idx;
+    }    
 };
