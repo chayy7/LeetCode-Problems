1568a757fcf4528ed279fdd11f305361a6bc6a2c
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-03-21 20:03:29 +0530

    Time: 0 ms (100%), Space: 8 MB (36.95%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
-
     int hammingDistance(int x, int y) {
-        
-        // string xBin = bitset<32>(x).to_string();
-        // string yBin = bitset<32>(y).to_string();
-        // int count = 0;
-        // for(int i=0;i<xBin.size();i++){
-        //     if(xBin[i] != yBin[i]){
-        //         count++;
-        //     }
-        // }
-        // return count;
         int xorNums = x ^ y;
         int ans = __builtin_popcount(xorNums);
         return ans;
