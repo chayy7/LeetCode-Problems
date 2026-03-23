9c667e8cf5af23cc7c7c469a8aed4c50b73acb82
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-03-23 21:23:05 +0530

    Time: 0 ms (100%), Space: 32.8 MB (5.72%) - LeetHub

--- 3417-zigzag-grid-traversal-with-skip/3417-zigzag-grid-traversal-with-skip.cpp
+++ 3417-zigzag-grid-traversal-with-skip/3417-zigzag-grid-traversal-with-skip.cpp
@@ -4,19 +4,25 @@ public:
         vector<int> ans;
         int row = arr.size();
         int col = arr[0].size();
+        vector<int> v;
         for(int i=0;i<row;i++){
-            if(i % 2 == 0){
-                for(int j=0;j<col;j++){
-                    if((i+j) % 2 == 1) continue;
-                    ans.push_back(arr[i][j]);
-                }
-            }else{
-                for(int j=col-1;j>=0;j--){
-                    if((i+j) % 2 == 1) continue;
-                    ans.push_back(arr[i][j]);
-                }
+            if(i % 2 != 0){
+                reverse(arr[i].begin(), arr[i].end());
+            }
+            
+            for(auto k:arr[i]){
+                
+                // cout << k << " ";
+                v.push_back(k);
             }
         }
+        
+        int i=0;
+        while(i < v.size()){
+            ans.push_back(v[i]);
+            i+=2;
+        }
+        
         return ans;
     }
 };
