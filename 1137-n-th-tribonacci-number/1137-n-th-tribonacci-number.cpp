60d0f20d8a01cfdbe42dc25c037bea0d3686f1b2
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-03-24 09:53:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (7.09%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int func(int n, vector<int>& dp){
-        if(n == 0) return 0;
-        if(n == 1 or n==2) return 1;
+    // int func(int n, vector<int>& dp){
+    //     if(n == 0) return 0;
+    //     if(n == 1 or n==2) return 1;
 
-        if(dp[n] != -1){
-            return dp[n];
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+
+    //     return dp[n] = func(n-1, dp)+func(n-2,dp) + func(n-3,dp);
+    // }
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+         if(n == 1 or n==2) return 1;
+        vector<int> dp(n+1, -1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<n+1;i++){
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        // return func(n, dp);
+
+        for(auto i:dp){
+            cout << i << " ";
         }
 
-        return dp[n] = func(n-1, dp)+func(n-2,dp) + func(n-3,dp);
-    }
-    int tribonacci(int n) {
-        vector<int> dp(n+1, -1);
-        // dp[0]=0;
-        // dp[1]=1;
-        // dp[2]=1;
-        // for(int i=3;i<=n;i++){
-        //     dp[n] = dp[n-1]+dp[n-2]+dp[n-3];
-        // }
-        return func(n, dp);
+        return dp[n];
         
 
     }
