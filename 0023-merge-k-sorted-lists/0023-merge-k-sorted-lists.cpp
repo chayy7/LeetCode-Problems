b4a7cfe8cba57d963464419daf36d5bb8a13c8ee
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-05-20 21:16:18 +0530

    Time: 2 ms (67.76%), Space: 19.3 MB (8.32%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,42 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+    void getArr(ListNode* head, vector<int> &arr){
+        ListNode* temp = head;
+        while(temp){
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> v;
-
-        for(auto i:lists){
-            ListNode* temp= i;
-            while(temp){
-                v.push_back(temp->val);
-                temp=temp->next;
-            }
-        }
-        sort(v.begin(),v.end());
-        for(auto i: v){
-            cout << i << " ";
-        }
-
-
-        if(v.empty()) return nullptr;
-        ListNode* head= new ListNode(v[0]);
-        ListNode* curr= head;
+        vector<int> arr;
         
-        for(int i=1;i<v.size();i++){
-            curr->next= new ListNode(v[i]);
-            curr=curr->next;
+        int row = lists.size();
+        for(int i=0;i<row;i++){
+            getArr(lists[i], arr);
         }
-
-
-
-        return head;
+        sort(arr.begin(), arr.end());
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        for(auto i: arr){
+            temp->next = new ListNode(i);
+            temp = temp->next;
+        }
+        return dummy->next;
     }
 };
