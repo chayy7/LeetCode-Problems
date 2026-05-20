ed18a7ce6fb61ce50093a94b59f492954880f9fa
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-05-20 21:39:36 +0530

    Time: 0 ms (100%), Space: 15 MB (8.71%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -1,52 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* partition(ListNode* head, int x) {
-        ListNode* temp=head;
-        vector<int> v;
+    void getArr(ListNode* head, vector<int> &arr){
+        ListNode* temp = head;
         while(temp){
-            v.push_back(temp->val);
-            temp=temp->next;
+            arr.push_back(temp->val);
+            temp = temp ->next;
         }
-
-
-        vector<int> ls;
-        vector<int> gs;
-        for(auto i:v){
+    }
+    ListNode* partition(ListNode* head, int x) {
+        vector<int> arr;
+        getArr(head, arr);
+        
+        
+        vector<int>left, right;
+        for(auto i: arr){
             if(i < x){
-                ls.push_back(i);
+                left.push_back(i);
             }else{
-                gs.push_back(i);
+                right.push_back(i);
             }
         }
-
-        vector<int> ans;
-        for(auto i:ls){
-            ans.push_back(i);
+        
+        ListNode* temp = head;
+        for(auto i: left){
+            temp->val = i;
+            temp = temp->next;
         }
-        for(auto i:gs){
-            ans.push_back(i);
-        }
-
-
-        for(auto i:ans){
-            cout <<  i << " ";
-        }
-
-        int i=0;
-        temp=head;
-        while(temp){
-            temp->val=ans[i++];
-            temp=temp->next;
+        for(auto i: right){
+            temp->val = i;
+            temp = temp->next;
         }
         return head;
     }
