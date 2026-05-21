8a7831282d8e1c814c1cfedc4657d5ff1337f2e8
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-05-21 21:19:32 +0530

    Time: 0 ms (100%), Space: 125.4 MB (89.3%) - LeetHub

--- 3909-compare-sums-of-bitonic-parts/3909-compare-sums-of-bitonic-parts.cpp
+++ 3909-compare-sums-of-bitonic-parts/3909-compare-sums-of-bitonic-parts.cpp
@@ -1,35 +1,25 @@
-// #define long long int 
 class Solution {
 public:
     int compareBitonicSums(vector<int>& nums) {
-        int maxi = *max_element(nums.begin(), nums.end());
-
-        long long ls = 0;
-        long long rs = 0;
-        int stop = -1;
+        int peak = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i] != maxi){
-                ls += nums[i];
-            }else{
-                ls += maxi;
-                stop= i;
-                break;
+            if(nums[i] > nums[peak]){
+                peak = i;
             }
         }
-        for(int i=stop;i<nums.size();i++){
-            if(nums[i] != maxi){
-                rs += nums[i];
-            }else{
-                rs += maxi;
-            }
+        
+        long long asc = 0;
+        long long dec = 0;
+        for(int i=0;i<=peak;i++){
+            asc += nums[i];
         }
-        if(ls > rs){
-            return 0;
-        }else if(ls < rs){
-            return 1;
+        for(int i=peak;i<nums.size();i++){
+            dec += nums[i];
         }
-
+        
+        if(asc > dec) return 0;
+        if(asc < dec) return 1;
         return -1;
-
+        
     }
 };
