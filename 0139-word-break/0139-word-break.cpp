e25c459fd0605f70fa5629d8d8c30b59f764e1b2
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-05-23 22:26:27 +0530

    Time: 51 ms (5.06%), Space: 30.4 MB (5.04%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,49 +1,35 @@
 class Solution {
 public:
-
     bool search(string part, vector<string>& wordDict){
-
-        for(auto i : wordDict){
-
+        for(auto i:wordDict){
             if(i == part){
                 return true;
             }
         }
-
         return false;
     }
-
-
-    bool solve(string s, vector<string>& wordDict,
-               vector<int>& dp){
-
-        int n = s.size();
-
+    bool solve(string s, vector<string>& wordDict, vector<int>& dp){
+        int n=s.size();
         if(n == 0) return true;
-
 
         if(dp[n] != -1) return dp[n];
 
-        for(int i = 1; i <= n; i++){
 
-            string firstpart = s.substr(0, i);
+
+        for(int i=1;i<=s.size();i++){
+            string firstpart = s.substr(0,i);
             string lastpart = s.substr(i);
 
-            if(search(firstpart, wordDict) &&
-               solve(lastpart, wordDict, dp)) {
-
+            if(search(firstpart, wordDict) && solve(lastpart, wordDict, dp)){
                 return dp[n] = true;
             }
         }
-
         return dp[n] = false;
     }
-
-
     bool wordBreak(string s, vector<string>& wordDict) {
 
-        vector<int> dp(s.size() + 1, -1);
+        vector<int> dp(s.size()+1, -1);
 
-        return solve(s, wordDict, dp);
+        return solve(s,wordDict, dp);
     }
 };
