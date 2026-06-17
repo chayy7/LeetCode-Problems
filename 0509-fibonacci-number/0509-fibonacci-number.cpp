d0d8952fb0e35a72efd473a49a624d172df9e7b5
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-06-17 20:00:45 +0530

    Time: 0 ms (100%), Space: 7.8 MB (37.99%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
-
-        vector<int> dp(max(2,n+1), -1);
-
-        dp[0] = 0, dp[1] = 1;
-
+        if(n == 0) return 0;
+        int prev2 = 0;
+        int prev1 = 1;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = curr;
+
         }
 
-        return dp[n];
+        return prev1;
+    
+        
     }
 };
