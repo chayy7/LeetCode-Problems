2ddd4ee6e4595aa7ca61f9230d340249e92c9c7b
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-06-29 22:57:39 +0530

    Time: 0 ms (100%), Space: 7.7 MB (58.14%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ans= "";
+
+        while(n > 0){
+            n--;
+            ans += 'A'+(n%26);
+            n/= 26;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
