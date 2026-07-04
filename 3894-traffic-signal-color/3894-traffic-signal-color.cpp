9316bf2a50a063447780096fef12346a8d283f51
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-07-04 18:03:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (8.94%) - LeetHub

---
+++ 3894-traffic-signal-color/3894-traffic-signal-color.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string trafficSignal(int t) {
+        if(t == 0){
+            return "Green";
+        }else if(t == 30){
+            return "Orange";
+        }else if(t > 30 && t <= 90){
+            return "Red";
+        }
+
+        return "Invalid";
+    }
+};
