5ed54c1fcba611dea32e18071de5c6b2c9260aa3
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-07-05 21:40:24 +0530

    Time: 35 ms (31.53%), Space: 32.2 MB (9.01%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        int cnt = 0;
-        map<char,int> mpp1;
-        for(auto i:s) mpp1[i]++;
-        map<char,int> mpp2;
-        for(auto i:t) mpp2[i]++;
-
-        for(int i='a'; i<='z';i++){
-            cnt += abs(mpp1[i] - mpp2[i]);
+        unordered_map<char, int> mpp1; 
+        unordered_map<char, int> mpp2;
+        for(auto i: s){
+            mpp1[i]++;
         }
+        for(auto i: t){
+            mpp2[i]++;
+        }
+        
+        int ans = 0;
+        for(int i='a';i<='z';i++){
+            ans += abs(mpp1[i] - mpp2[i]);
+        }
+        return ans;
+        
 
-        return cnt;
+        
+        
+        
     }
 };
