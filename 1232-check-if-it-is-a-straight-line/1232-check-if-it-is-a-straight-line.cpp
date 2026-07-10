a1838861266cf00b1bfd8924ca6fbaabb81da72d
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-07-10 18:39:20 +0530

    Time: 0 ms (100%), Space: 14.1 MB (13.91%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& c) {
+        if(c[0][0] == c[1][0]) {
+            for (int i = 2; i < c.size(); i++) {
+                if (c[i][0] != c[0][0])
+                    return false;
+            }
+            return true;
+        }
+
+        double slope = (double)(c[1][1] - c[0][1]) / (c[1][0] - c[0][0]);
+
+        for (int i = 2; i < c.size(); i++) {
+            double curr = (double)(c[i][1] - c[0][1]) / (c[i][0] - c[0][0]);
+
+            if (curr != slope)
+                return false;
+        }
+
+        return true;
+    }
+};
