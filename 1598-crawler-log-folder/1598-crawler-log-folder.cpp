701be6e50c44208112f38fb1275473441d906266
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-07-11 21:58:59 +0530

    Time: 2 ms (12.42%), Space: 14.1 MB (61.83%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        int cnt = 0;
-        for(auto i:logs){
-            if(i == "../" && cnt > 0) cnt--;
-            else if(i == "./") continue;
-            else if(isalnum(i[0])) cnt++;
+        int depth = 0;
+        for(auto i: logs){
+            if(i != "./" && i != "../"){
+                depth++; 
+            }
+            if(i == "../" && depth > 0){
+                depth--;
+            }
         }
-        return cnt < 0? 0:cnt;
+        
+        return depth;
     }
 };
