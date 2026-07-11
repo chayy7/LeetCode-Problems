42babc6ae53b9f36728e5e4b6143a9e155ca9602
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-07-11 22:17:14 +0530

    Time: 0 ms (100%), Space: 14 MB (50.23%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& c) {
-        if(c[0][0] == c[1][0]) {
-            for (int i = 2; i < c.size(); i++) {
-                if (c[i][0] != c[0][0])
-                    return false;
+        // 2nd round of KUNAFA!
+        if(c[0][0] == c[1][0]){
+            for(int i=2;i<c.size();i++){
+                if(c[i][0] != c[0][0]) return false;
             }
             return true;
         }
+        
+        
 
-        double slope = (double)(c[1][1] - c[0][1]) / (c[1][0] - c[0][0]);
+        int row = c.size();
+        int col = c[0].size();
+        int dx = c[1][0] - c[0][0];
+        int dy = c[1][1] - c[0][1];
+        double slope = (double)dy / dx;
 
-        for (int i = 2; i < c.size(); i++) {
-            double curr = (double)(c[i][1] - c[0][1]) / (c[i][0] - c[0][0]);
+        for (int i = 2; i < row; i++) {
 
-            if (curr != slope){
+            int cdx = c[i][0] - c[0][0];
+            int cdy = c[i][1] - c[0][1];
+            double currSlope = (double)cdy / cdx;
+
+            if (slope != currSlope)
                 return false;
-            }
         }
 
         return true;
