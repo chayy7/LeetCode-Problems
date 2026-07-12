d1d1c1e155e8d1db2242f71fc8bb5f561ea96aff
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-07-12 08:47:38 +0530

    Time: 3 ms (6.85%), Space: 15.1 MB (8.04%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int amount(vector<int>& nums, int i, vector<int>& dp){
-        if(i >= nums.size())return 0;
-        if(dp[i] != -1)return dp[i];
-        int take = nums[i] + amount(nums, i+2, dp);
-        int skip = amount(nums, i+1,dp);
-        return dp[i] = max(take,  skip);
+    int func(vector<int>& dp, vector<int> v, int n){
+        if(n == 0){
+            dp[n] = v[n];
+            return v[n];
+        }
+
+        if(n < 0) return 0;
+
+
+        if(dp[n] != -1) return dp[n];
+
+        int pick = v[n]+ func(dp, v, n-2);
+        int npick = func(dp, v, n-1);
+
+        return dp[n] = max(pick, npick);
     }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return nums[0];
-        vector<int>v1(nums.begin(), nums.end()-1);
-        vector<int> dp(v1.size(), -1);
-        vector<int>v2(nums.begin()+1, nums.end());
-        vector<int> dp1(v2.size(), -1);
-        
-        int a= amount(v1, 0, dp);
-        int b= amount(v2, 0, dp1);
-        return max(a,b);
+        int n=nums.size();
+
+         if (n == 1)
+            return nums[0];
+
+        vector<int> v1(nums.begin(), nums.end()-1);
+        vector<int> v2(nums.begin()+1, nums.end());
+
+        vector<int> dp1(v1.size(), -1);
+        vector<int> dp2(v2.size(), -1);
+
+        int func1 = func(dp1, v1, v1.size()-1);
+        int func2 = func(dp2, v2, v2.size()-1);
+
+        return max(func1, func2);
     }
 };
