3525a00043ddc412a66d5bcc191bb363096bcbad
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-07-12 08:33:48 +0530

    Time: 3 ms (5.99%), Space: 12.9 MB (6.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int func(vector<int>& nums,int n, vector<int>& dp){
+    int func(int n, vector<int>& dp, vector<int> nums){
+        if(n == 0){
+            dp[n] = nums[n];
+            return nums[n];
+        }
+
         if(n < 0) return 0;
 
-        if(dp[n] != -1) return dp[n];
+        if(dp[n] != -1 )return dp[n];
 
-        int pick= nums[n] + func(nums,n-2,dp);
-        int npick= func(nums,n-1,dp);
+        int pick = nums[n] + func(n-2, dp, nums);
+        int npick = func(n-1, dp, nums);
 
-        return dp[n]= max(pick, npick);
-
+        return dp[n] = max(pick, npick);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n, -1);
-        return func(nums, n-1, dp);
+
+        vector<int> dp(n+1, -1);
+        
+
+
+        return func(n-1 , dp, nums);
     }
 };
