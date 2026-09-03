a09251b6d0957d55bec56fce9541350855365728
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-09-03 06:49:07 +0530

    Time: 0 ms (100%), Space: 91.9 MB (99.6%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int n=nums.size();
-        int mini = *min_element(nums.begin(), nums.end());
-        int maxi = *max_element(nums.begin(), nums.end());
+       int n= nums.size();
 
-        int mini_idx= -1;
-        int maxi_idx= -1;
+       if(n <= 2) return n;
 
-        for(int i=0;i<n;i++){
-            if(nums[i] == mini){
-                mini_idx = i;
-            }
-            if(nums[i] == maxi){
-                maxi_idx = i;
-            }
-        }
-
-        // int mini_bidx = -1;
-        // int maxi_bidx = -1;
-
-        int mini_bidx = n- mini_idx ;
-        int maxi_bidx = n-maxi_idx;
+       int minp = 0;
+       int maxp = 0;
+       for(int i=1;i<n;++i){
+            if(nums[i] < nums[minp]) minp=i; 
+            if(nums[i] > nums[maxp]) maxp=i; 
+       }
 
 
+       int l= min(minp, maxp);
+       int r= max(minp, maxp);
 
+       int p1 = r+1;
+       int p2 = n-l;
 
-        cout << mini_idx << endl;
-        cout << maxi_idx << endl;
-        cout << mini_bidx << endl;
-        cout << maxi_bidx << endl;
+       int p3 = (l+1) + (n-r);
 
-        int ff = max(mini_idx+1, maxi_idx+1);
-        int bb = max(mini_bidx, maxi_bidx);
-        int fb = min(mini_idx+1+ maxi_bidx, mini_bidx+1+maxi_idx);
+       return min(min(p1,p2),p3);
 
-
-        return min(ff,min(bb,fb));
     }
 };
