403c5bb337d14e0e26a86c295d7f4fd621cbda47
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-09-07 20:21:33 +0530

    Time: 0 ms (100%), Space: 8.9 MB (92.47%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int findPermutationDifference(string s, string t) {
-        map<char,int> mpp1;
+        int ans = 0;
         for(int i=0;i<s.size();i++){
-            mpp1[s[i]] = i;
+            int a = i;
+            int b = find(t.begin(), t.end(), s[i]) - t.begin();
+            int dif = abs(a-b);
+            ans += dif;
         }
-
-        for(auto i:mpp1) cout << i.first << " " << i.second << endl;
-        map<char,int> mpp2;
-        for(int i=0;i<t.size();i++){
-            mpp2[t[i]] = i;
-        }
-        for(auto i:mpp2) cout << i.first << " " << i.second << endl;
-
-        int sum = 0;
-        for(int i=0;i<s.size();i++){
-            sum += abs(mpp1[s[i]] - mpp2[s[i]]);
-        }
-
-        return sum;
+        return ans;
     }
 };
