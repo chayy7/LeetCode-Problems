65672f890c3952bdfc26d40ab1f6c44875c70277
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-09-08 19:57:41 +0530

    Time: 0 ms (100%), Space: 8.8 MB (7.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int countCommas(int n) {
+        int len = int(log10(n))+1;
+
         int cnt = 0;
-        for(int i=1000;i <= n;i++){
-            string s=to_string(i);
-            cnt += (s.size()-1)/3;
-        }
+       for(int i =1000; i<= n;i++){
+            cnt++;
+       }
         return cnt;
     }
 };
